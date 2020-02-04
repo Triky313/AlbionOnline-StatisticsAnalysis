6598a238a17c597269bfb61a41c9fda3f365695c
Aaron Schultz <a.schultz@tech2go.de>
2020-02-04 10:07:38 +0100

    Refactoring

--- StatisticsAnalysisTool/StatisticsAnalysisTool/MainWindow.xaml.cs
+++ StatisticsAnalysisTool/StatisticsAnalysisTool/MainWindow.xaml.cs
@@ -1,13 +1,9 @@
 ﻿using FontAwesome.WPF;
-using StatisticsAnalysisTool.Common;
 using StatisticsAnalysisTool.Models;
 using StatisticsAnalysisTool.Properties;
 using StatisticsAnalysisTool.ViewModels;
 using System.ComponentModel;
 using System.Diagnostics;
-using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -31,100 +27,13 @@ namespace StatisticsAnalysisTool
         public MainWindow()
         {
             InitializeComponent();
-            Utilities.AutoUpdate();
-            InitMarketAnalysis();
             _mainWindowViewModel = new MainWindowViewModel(this);
             DataContext = _mainWindowViewModel;
         }
 
-        private void InitMarketAnalysis()
-        {
-            Task.Run(async () =>
-            {
-                Dispatcher?.Invoke(() =>
-                {
-                    TxtSearch.IsEnabled = false;
-                    FaLoadIcon.Visibility = Visibility.Visible;
-                    InitUi();
-                });
-
-                #region Set MainWindow height and width and center window
-
-                Dispatcher?.Invoke(() =>
-                {
-                    Height = Settings.Default.MainWindowHeight;
-                    Width = Settings.Default.MainWindowWidth;
-                    if (Settings.Default.MainWindowMaximized)
-                    {
-                        WindowState = WindowState.Maximized;
-                    }
-
-                    CenterWindowOnScreen();
-                });
-
-                #endregion
-
-                Dispatcher?.Invoke(() =>
-                {
-                    TxtBoxPlayerModeUsername.Text = Settings.Default.SavedPlayerInformationName;
-                });
-
-                var isItemListLoaded = await StatisticsAnalysisManager.GetItemListFromJsonAsync();
-                if (!isItemListLoaded)
-                    MessageBox.Show(LanguageController.Translation("ITEM_LIST_CAN_NOT_BE_LOADED"), 
-                        LanguageController.Translation("ERROR"));
-
-                Dispatcher?.Invoke(() =>
-                {
-                    if(isItemListLoaded)
-                    {
-                        FaLoadIcon.Visibility = Visibility.Hidden;
-                        TxtSearch.IsEnabled = true;
-                        TxtSearch.Focus();
-                    }
-                });
-            });
-        }
-
-        private void InitUi()
-        {
-            LblToolName.Content = $"AlbionOnline - STATISTICS ANALYSIS TOOL | v{Assembly.GetExecutingAssembly().GetName().Version}";
-
-            CbMode.Items.Clear();
-            CbMode.Items.Add(new ComboboxMarketMode { Name = LanguageController.Translation("NORMAL"), Mode = ViewMode.Normal });
-            CbMode.Items.Add(new ComboboxMarketMode { Name = LanguageController.Translation("PLAYER"), Mode = ViewMode.Player });
-
-            if (CbMode.Items.Count > 0)
-                CbMode.SelectedIndex = 0;
-        }
-
-        public void LoadLvItems(string searchText)
-        {
-            if (string.IsNullOrEmpty(searchText))
-                return;
-
-            Dispatcher?.InvokeAsync(async () =>
-            {
-                var items = await StatisticsAnalysisManager.FindItemsAsync(searchText);
-                LvItems.ItemsSource = items;
-                LblItemCounter.Content = $"{items.Count}/{StatisticsAnalysisManager.Items.Count}";
-                LblLocalImageCounter.Content = ImageController.LocalImagesCounter();
-            });
-        }
-
-        private void CenterWindowOnScreen()
-        {
-            var screenWidth = SystemParameters.PrimaryScreenWidth;
-            var screenHeight = SystemParameters.PrimaryScreenHeight;
-            var windowWidth = Width;
-            var windowHeight = Height;
-            Left = (screenWidth / 2) - (windowWidth / 2);
-            Top = (screenHeight / 2) - (windowHeight / 2);
-        }
-
         private void TxtSearch_KeyUp(object sender, KeyEventArgs e)
         {
-            LoadLvItems(TxtSearch.Text);
+            _mainWindowViewModel.LoadLvItems(TxtSearch.Text);
         }
 
         private void LvItems_MouseDoubleClick(object sender, MouseButtonEventArgs e)
@@ -190,7 +99,7 @@ namespace StatisticsAnalysisTool
                 Top = _storedSize.Top;
                 Height = _storedSize.Height;
                 Width = _storedSize.Width;
-                CenterWindowOnScreen();
+                _mainWindowViewModel.CenterWindowOnScreen();
                 MaximizedButton.Content = 1;
                 _isWindowMaximized = false;
             }
@@ -256,23 +165,9 @@ namespace StatisticsAnalysisTool
             Settings.Default.Save();
         }
 
-        private async Task LoadPlayerInformation()
-        {
-            if (string.IsNullOrWhiteSpace(TxtBoxPlayerModeUsername.Text))
-                return;
-
-            _mainWindowViewModel.GameInfoSearch = await ApiController.GetGameInfoSearchFromJsonAsync(TxtBoxPlayerModeUsername.Text);
-
-            if (_mainWindowViewModel.GameInfoSearch?.SearchPlayer?.FirstOrDefault()?.Id == null)
-                return;
-
-            _mainWindowViewModel.SearchPlayer = _mainWindowViewModel.GameInfoSearch?.SearchPlayer?.FirstOrDefault();
-            _mainWindowViewModel.GameInfoPlayers = await ApiController.GetGameInfoPlayersFromJsonAsync(_mainWindowViewModel.GameInfoSearch?.SearchPlayer?.FirstOrDefault()?.Id);
-        }
-        
         private async void BtnPlayerModeSave_Click(object sender, RoutedEventArgs e)
         {
-            await LoadPlayerInformation();
+            await _mainWindowViewModel.LoadPlayerInformation();
         }
 
         private async void TxtBoxPlayerModeUsername_KeyDown(object sender, KeyEventArgs e)
@@ -280,7 +175,7 @@ namespace StatisticsAnalysisTool
             if (e.Key != Key.Enter)
                 return;
 
-            await LoadPlayerInformation();
+            await _mainWindowViewModel.LoadPlayerInformation();
         }
     }
 }
--- StatisticsAnalysisTool/StatisticsAnalysisTool/ViewModels/MainWindowViewModel.cs
+++ StatisticsAnalysisTool/StatisticsAnalysisTool/ViewModels/MainWindowViewModel.cs
@@ -7,6 +7,10 @@ using System.Windows;
 
 namespace StatisticsAnalysisTool.ViewModels
 {
+    using System.Linq;
+    using System.Reflection;
+    using System.Threading.Tasks;
+
     public class MainWindowViewModel : INotifyPropertyChanged
     {
         private readonly MainWindow _mainWindow;
@@ -18,8 +22,10 @@ namespace StatisticsAnalysisTool.ViewModels
         public MainWindowViewModel(MainWindow mainWindow)
         {
             _mainWindow = mainWindow;
+            Utilities.AutoUpdate();
             UpgradeSettings();
             InitLanguage();
+            InitMainWindowData();
         }
 
         private void UpgradeSettings()
@@ -39,6 +45,96 @@ namespace StatisticsAnalysisTool.ViewModels
 
             MessageBox.Show("ERROR: No language file found!");
             _mainWindow.Close();
+        }
+
+        private void InitMainWindowData()
+        {
+            Task.Run(async () =>
+            {
+                _mainWindow.Dispatcher?.Invoke(() =>
+                {
+                    _mainWindow.TxtSearch.IsEnabled = false;
+                    _mainWindow.FaLoadIcon.Visibility = Visibility.Visible;
+
+                    #region Set combobox mode
+
+                    _mainWindow.CbMode.Items.Clear();
+                    _mainWindow.CbMode.Items.Add(new ComboboxMarketMode { Name = LanguageController.Translation("NORMAL"), Mode = MainWindow.ViewMode.Normal });
+                    _mainWindow.CbMode.Items.Add(new ComboboxMarketMode { Name = LanguageController.Translation("PLAYER"), Mode = MainWindow.ViewMode.Player });
+
+                    if (_mainWindow.CbMode.Items.Count > 0)
+                        _mainWindow.CbMode.SelectedIndex = 0;
+
+                    #endregion
+
+                    #region Set MainWindow height and width and center window
+
+                    _mainWindow.Height = Settings.Default.MainWindowHeight;
+                    _mainWindow.Width = Settings.Default.MainWindowWidth;
+                    if (Settings.Default.MainWindowMaximized)
+                    {
+                        _mainWindow.WindowState = WindowState.Maximized;
+                    }
+
+                    CenterWindowOnScreen();
+                    #endregion
+
+                    _mainWindow.TxtBoxPlayerModeUsername.Text = Settings.Default.SavedPlayerInformationName;
+                });
+                
+                var isItemListLoaded = await StatisticsAnalysisManager.GetItemListFromJsonAsync().ConfigureAwait(true);
+                if (!isItemListLoaded)
+                    MessageBox.Show(LanguageController.Translation("ITEM_LIST_CAN_NOT_BE_LOADED"),
+                        LanguageController.Translation("ERROR"));
+
+                _mainWindow.Dispatcher?.Invoke(() =>
+                {
+                    if (isItemListLoaded)
+                    {
+                        _mainWindow.FaLoadIcon.Visibility = Visibility.Hidden;
+                        _mainWindow.TxtSearch.IsEnabled = true;
+                        _mainWindow.TxtSearch.Focus();
+                    }
+                });
+            });
+        }
+
+        public void CenterWindowOnScreen()
+        {
+            var screenWidth = SystemParameters.PrimaryScreenWidth;
+            var screenHeight = SystemParameters.PrimaryScreenHeight;
+            var windowWidth = _mainWindow.Width;
+            var windowHeight = _mainWindow.Height;
+            _mainWindow.Left = (screenWidth / 2) - (windowWidth / 2);
+            _mainWindow.Top = (screenHeight / 2) - (windowHeight / 2);
+        }
+
+        public void LoadLvItems(string searchText)
+        {
+            if (string.IsNullOrEmpty(searchText))
+                return;
+
+            _mainWindow.Dispatcher?.InvokeAsync(async () =>
+            {
+                var items = await StatisticsAnalysisManager.FindItemsAsync(searchText);
+                _mainWindow.LvItems.ItemsSource = items;
+                _mainWindow.LblItemCounter.Content = $"{items.Count}/{StatisticsAnalysisManager.Items.Count}";
+                _mainWindow.LblLocalImageCounter.Content = ImageController.LocalImagesCounter();
+            });
+        }
+
+        public async Task LoadPlayerInformation()
+        {
+            if (string.IsNullOrWhiteSpace(_mainWindow.TxtBoxPlayerModeUsername.Text))
+                return;
+
+            GameInfoSearch = await ApiController.GetGameInfoSearchFromJsonAsync(_mainWindow.TxtBoxPlayerModeUsername.Text);
+
+            if (GameInfoSearch?.SearchPlayer?.FirstOrDefault()?.Id == null)
+                return;
+
+            SearchPlayer = GameInfoSearch?.SearchPlayer?.FirstOrDefault();
+            GameInfoPlayers = await ApiController.GetGameInfoPlayersFromJsonAsync(GameInfoSearch?.SearchPlayer?.FirstOrDefault()?.Id);
         }
 
         public GameInfoSearchResponse GameInfoSearch {
@@ -70,6 +166,7 @@ namespace StatisticsAnalysisTool.ViewModels
         public string DonateUrl => Settings.Default.DonateUrl;
         public string SavedPlayerInformationName => Settings.Default.SavedPlayerInformationName ?? "";
         public string SaveTranslation => LanguageController.Translation("SAVE");
+        public string Version => $"v{Assembly.GetExecutingAssembly().GetName().Version}";
 
         public event PropertyChangedEventHandler PropertyChanged;
 
