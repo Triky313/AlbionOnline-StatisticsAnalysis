7fd5a6169443444f00db878f613ce33031f1cc6e
Aaron Schultz <schultzaaron@web.de>
2020-02-07 18:02:29 +0100

    Refactoring item window (MVVM)

--- StatisticsAnalysisTool/StatisticsAnalysisTool/ViewModels/ItemWindowViewModel.cs
+++ StatisticsAnalysisTool/StatisticsAnalysisTool/ViewModels/ItemWindowViewModel.cs
@@ -1,16 +1,280 @@
-﻿namespace StatisticsAnalysisTool.ViewModels
-{
-    using System.ComponentModel;
-    using System.Runtime.CompilerServices;
-    using Views;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Windows;
+using StatisticsAnalysisTool.Common;
+using StatisticsAnalysisTool.Models;
+using StatisticsAnalysisTool.Properties;
+using StatisticsAnalysisTool.Views;
 
+namespace StatisticsAnalysisTool.ViewModels
+{
     public class ItemWindowViewModel: INotifyPropertyChanged
     {
-        private ItemWindow _mainWindow;
+        private readonly ItemWindow _mainWindow;
+        private ItemInformation _itemData = new ItemInformation();
+        private Item _item;
+        private bool _runUpdate = true;
+        private bool _isAutoUpdateActive;
 
-        public ItemWindowViewModel(ItemWindow mainWindow)
+        public ItemWindowViewModel(ItemWindow mainWindow, Item item)
         {
             _mainWindow = mainWindow;
+            _item = item;
+
+            _mainWindow.LblItemName.Content = "";
+            _mainWindow.LblItemId.Content = "";
+            _mainWindow.LblLastUpdate.Content = "";
+
+            Translation();
+            InitializeItemData(item);
+
+            _mainWindow.ListViewPrices.Language = System.Windows.Markup.XmlLanguage.GetLanguage(LanguageController.DefaultCultureInfo.ToString());
+        }
+
+        private void Translation()
+        {
+            _mainWindow.ChbShowVillages.Content = LanguageController.Translation("SHOW_VILLAGES");
+            _mainWindow.ChbShowBlackZoneOutposts.Content = LanguageController.Translation("SHOW_BLACKZONE_OUTPOSTS");
+            _mainWindow.ChbAutoUpdateData.Content = LanguageController.Translation("AUTO_UPDATE_DATA");
+            _mainWindow.LblLastUpdate.ToolTip = LanguageController.Translation("LAST_UPDATE");
+            _mainWindow.GvcCityTitel.Header = LanguageController.Translation("CITY");
+            _mainWindow.GvcSellPriceMin.Header = LanguageController.Translation("SELL_PRICE_MIN");
+            _mainWindow.GvcSellPriceMinDate.Header = LanguageController.Translation("SELL_PRICE_MIN_DATE");
+            _mainWindow.GvcSellPriceMax.Header = LanguageController.Translation("SELL_PRICE_MAX");
+            _mainWindow.GvcSellPriceMaxDate.Header = LanguageController.Translation("SELL_PRICE_MAX_DATE");
+            _mainWindow.GvcBuyPriceMin.Header = LanguageController.Translation("BUY_PRICE_MIN");
+            _mainWindow.GvcBuyPriceMinDate.Header = LanguageController.Translation("BUY_PRICE_MIN_DATE");
+            _mainWindow.GvcBuyPriceMax.Header = LanguageController.Translation("BUY_PRICE_MAX");
+            _mainWindow.GvcBuyPriceMaxDate.Header = LanguageController.Translation("BUY_PRICE_MAX_DATE");
+            _mainWindow.LblDifCalcName.Content = $"{LanguageController.Translation("DIFFERENT_CALCULATION")}:";
+        }
+
+        private async void InitializeItemData(Item item)
+        {
+            if (item == null)
+                return;
+
+            if (_mainWindow.Dispatcher == null)
+                return;
+
+            await _mainWindow.Dispatcher.InvokeAsync(() =>
+            {
+                _mainWindow.FaLoadIcon.Visibility = Visibility.Visible;
+                _mainWindow.Icon = item.Icon;
+            });
+
+            StartAutoUpdater();
+
+            var itemDataTaskResult = await ApiController.GetItemInfoFromJsonAsync(item);
+
+            if (itemDataTaskResult == null)
+            {
+                // TODO: Refactoring
+                _mainWindow.LblItemName.Content = LanguageController.Translation("ERROR_PRICES_CAN_NOT_BE_LOADED");
+                _mainWindow.Dispatcher?.Invoke(() => { _mainWindow.FaLoadIcon.Visibility = Visibility.Hidden; });
+                return;
+            }
+
+            _itemData = itemDataTaskResult;
+
+            if (_mainWindow.Dispatcher == null)
+                return;
+
+            await _mainWindow.Dispatcher.InvokeAsync(() =>
+            {
+                _mainWindow.Title = $"{ItemController.LocalizedName(_itemData)} (T{_itemData.Tier})";
+                _mainWindow.LblItemName.Content = $"{ItemController.LocalizedName(_itemData)} (T{_itemData.Tier})";
+                _mainWindow.LblItemId.Content = _itemData.UniqueName;
+                _mainWindow.ImgItemImage.Source = item.Icon;
+
+                _mainWindow.FaLoadIcon.Visibility = Visibility.Hidden;
+            });
+
+        }
+
+        private async void StartAutoUpdater()
+        {
+            await Task.Run(async () => {
+                if (_isAutoUpdateActive)
+                    return;
+
+                _isAutoUpdateActive = true;
+                while (RunUpdate)
+                {
+                    await Task.Delay(500);
+                    if (_mainWindow.Dispatcher != null && _mainWindow.Dispatcher.Invoke(() => !_mainWindow.ChbAutoUpdateData.IsChecked ?? false))
+                        continue;
+
+                    GetPriceStats();
+                    await Task.Delay(Settings.Default.RefreshRate - 500);
+                }
+                _isAutoUpdateActive = false;
+            });
+        }
+
+        public async void GetPriceStats()
+        {
+            if (Item.UniqueName == null)
+                return;
+
+            await Task.Run(async () =>
+            {
+                var showVillagesIsChecked = _mainWindow.Dispatcher != null && _mainWindow.Dispatcher.Invoke(() => _mainWindow.ChbShowVillages.IsChecked ?? false);
+                var showBlackZoneOutpostsIsChecked = _mainWindow.Dispatcher != null && _mainWindow.Dispatcher.Invoke(() => _mainWindow.ChbShowBlackZoneOutposts.IsChecked ?? false);
+
+                var statPricesList = await ApiController.GetCityItemPricesFromJsonAsync(Item.UniqueName, Locations.GetLocationsListByArea(new IsLocationAreaActive()
+                {
+                    Cities = true,
+                    Villages = showVillagesIsChecked,
+                    BlackZoneOutposts = showBlackZoneOutpostsIsChecked
+                }));
+
+                if (statPricesList == null)
+                    return;
+
+                var statsPricesTotalList = PriceUpdate(statPricesList);
+
+                FindBestPrice(ref statsPricesTotalList);
+
+                var marketCurrentPricesItemList = new List<MarketCurrentPricesItem>();
+                foreach (var item in statsPricesTotalList)
+                    marketCurrentPricesItemList.Add(new MarketCurrentPricesItem(item));
+
+                _mainWindow.Dispatcher?.Invoke(() =>
+                {
+                    _mainWindow.ListViewPrices.ItemsSource = marketCurrentPricesItemList;
+                    SetDifferenceCalculationText(statsPricesTotalList);
+                    _mainWindow.LblLastUpdate.Content = Utilities.DateFormat(DateTime.Now, 0);
+                });
+            });
+        }
+
+        private List<MarketResponseTotal> PriceUpdate(List<MarketResponse> statPricesList)
+        {
+            var statsPricesTotalList = new List<MarketResponseTotal>();
+
+            foreach (var stats in statPricesList)
+            {
+                if (statsPricesTotalList.Exists(s => Locations.GetParameterName(s.City) == stats.City))
+                {
+                    var spt = statsPricesTotalList.Find(s => Locations.GetName(s.City) == stats.City);
+                    if (stats.SellPriceMin < spt.SellPriceMin)
+                        spt.SellPriceMin = stats.SellPriceMin;
+
+                    if (stats.SellPriceMax > spt.SellPriceMax)
+                        spt.SellPriceMax = stats.SellPriceMax;
+
+                    if (stats.BuyPriceMin < spt.BuyPriceMin)
+                        spt.BuyPriceMin = stats.BuyPriceMin;
+
+                    if (stats.BuyPriceMax > spt.BuyPriceMax)
+                        spt.BuyPriceMax = stats.BuyPriceMax;
+                }
+                else
+                {
+                    statsPricesTotalList.Add(new MarketResponseTotal(stats));
+                }
+            }
+
+            return statsPricesTotalList;
+        }
+
+        private void FindBestPrice(ref List<MarketResponseTotal> list)
+        {
+            if (list.Count == 0)
+                return;
+
+            var max = GetMaxPrice(list);
+
+            try
+            {
+                list.Find(s => s.BuyPriceMax == max).BestBuyMaxPrice = true;
+            }
+            catch (Exception ex)
+            {
+                Debug.Print(ex.ToString());
+            }
+
+            var min = GetMinPrice(list);
+
+            try
+            {
+                list.Find(s => s.SellPriceMin == min).BestSellMinPrice = true;
+            }
+            catch (Exception ex)
+            {
+                Debug.Print(ex.ToString());
+            }
+
+        }
+
+        private static ulong GetMaxPrice(List<MarketResponseTotal> list)
+        {
+            var max = ulong.MinValue;
+            foreach (var type in list)
+            {
+                if (type.BuyPriceMax == 0)
+                    continue;
+
+                if (type.BuyPriceMax > max)
+                    max = type.BuyPriceMax;
+            }
+
+            return max;
+        }
+
+        private static ulong GetMinPrice(List<MarketResponseTotal> list)
+        {
+            var min = ulong.MaxValue;
+            foreach (var type in list)
+            {
+                if (type.SellPriceMin == 0)
+                    continue;
+
+                if (type.SellPriceMin < min)
+                    min = type.SellPriceMin;
+            }
+
+            return min;
+        }
+
+        private void SetDifferenceCalculationText(List<MarketResponseTotal> statsPricesTotalList)
+        {
+            ulong? bestBuyMaxPrice = 0UL;
+            ulong? bestSellMinPrice = 0UL;
+
+            if (statsPricesTotalList?.Count > 0)
+            {
+                bestBuyMaxPrice = statsPricesTotalList.FirstOrDefault(s => s.BestBuyMaxPrice)?.BuyPriceMax ?? 0UL;
+                bestSellMinPrice = statsPricesTotalList.FirstOrDefault(s => s.BestSellMinPrice)?.SellPriceMin ?? 0UL;
+            }
+
+            var diffPrice = (int)bestBuyMaxPrice - (int)bestSellMinPrice;
+
+            _mainWindow.LblDifCalcText.Content = $"{LanguageController.Translation("BOUGHT_FOR")} {string.Format(LanguageController.DefaultCultureInfo, "{0:n0}", bestSellMinPrice)} | " +
+                                                 $"{LanguageController.Translation("SELL_FOR")} {string.Format(LanguageController.DefaultCultureInfo, "{0:n0}", bestBuyMaxPrice)} | " +
+                                                 $"{LanguageController.Translation("PROFIT")} {string.Format(LanguageController.DefaultCultureInfo, "{0:n0}", diffPrice)}";
+        }
+
+        public Item Item {
+            get => _item;
+            set {
+                _item = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool RunUpdate {
+            get => _runUpdate;
+            set {
+                _runUpdate = value;
+                OnPropertyChanged();
+            }
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
--- StatisticsAnalysisTool/StatisticsAnalysisTool/Views/ItemWindow.xaml.cs
+++ StatisticsAnalysisTool/StatisticsAnalysisTool/Views/ItemWindow.xaml.cs
@@ -1,13 +1,7 @@
 ﻿namespace StatisticsAnalysisTool.Views
 {
-    using Common;
     using Models;
-    using Properties;
-    using System;
-    using System.Collections.Generic;
     using System.Diagnostics;
-    using System.Linq;
-    using System.Threading.Tasks;
     using System.Windows;
     using System.Windows.Input;
     using ViewModels;
@@ -20,254 +14,13 @@
 
         private readonly ItemWindowViewModel _itemWindowViewModel;
 
-        private ItemInformation _itemData =  new ItemInformation();
-        private string _uniqueName;
-        private bool _runUpdate = true;
-        private bool _isAutoUpdateActive;
-
         public ItemWindow(Item item)
         {
             InitializeComponent();
-            _itemWindowViewModel = new ItemWindowViewModel(this);
+            _itemWindowViewModel = new ItemWindowViewModel(this, item);
             DataContext = _itemWindowViewModel;
-
-            LblItemName.Content = "";
-            LblItemId.Content = "";
-            LblLastUpdate.Content = "";
-
-            Translation();
-            InitializeItemData(item);
-
-            ListViewPrices.Language = System.Windows.Markup.XmlLanguage.GetLanguage(LanguageController.DefaultCultureInfo.ToString());
-        }
-        
-        private void Translation()
-        {
-            ChbShowVillages.Content = LanguageController.Translation("SHOW_VILLAGES");
-            ChbShowBlackZoneOutposts.Content = LanguageController.Translation("SHOW_BLACKZONE_OUTPOSTS");
-            ChbAutoUpdateData.Content = LanguageController.Translation("AUTO_UPDATE_DATA");
-            LblLastUpdate.ToolTip = LanguageController.Translation("LAST_UPDATE");
-            GvcCityTitel.Header = LanguageController.Translation("CITY");
-            GvcSellPriceMin.Header = LanguageController.Translation("SELL_PRICE_MIN");
-            GvcSellPriceMinDate.Header = LanguageController.Translation("SELL_PRICE_MIN_DATE");
-            GvcSellPriceMax.Header = LanguageController.Translation("SELL_PRICE_MAX");
-            GvcSellPriceMaxDate.Header = LanguageController.Translation("SELL_PRICE_MAX_DATE");
-            GvcBuyPriceMin.Header = LanguageController.Translation("BUY_PRICE_MIN");
-            GvcBuyPriceMinDate.Header = LanguageController.Translation("BUY_PRICE_MIN_DATE");
-            GvcBuyPriceMax.Header = LanguageController.Translation("BUY_PRICE_MAX");
-            GvcBuyPriceMaxDate.Header = LanguageController.Translation("BUY_PRICE_MAX_DATE");
-            LblDifCalcName.Content = $"{LanguageController.Translation("DIFFERENT_CALCULATION")}:";
         }
 
-        private async void InitializeItemData(Item item)
-        {
-            if (item == null)
-                return;
-
-            _uniqueName = item.UniqueName;
-
-            if (Dispatcher == null)
-                return;
-
-            await Dispatcher.InvokeAsync(() =>
-            {
-                FaLoadIcon.Visibility = Visibility.Visible;
-                Icon = item.Icon;
-            });
-
-            StartAutoUpdater();
-
-            var itemDataTaskResult = await ApiController.GetItemInfoFromJsonAsync(item);
-
-            if (itemDataTaskResult == null)
-            {
-                // TODO: Refactoring
-                LblItemName.Content = LanguageController.Translation("ERROR_PRICES_CAN_NOT_BE_LOADED");
-                Dispatcher?.Invoke(() => { FaLoadIcon.Visibility = Visibility.Hidden; });
-                return;
-            }
-
-            _itemData = itemDataTaskResult;
-
-            if (Dispatcher == null)
-                return;
-
-            await Dispatcher.InvokeAsync(() =>
-            {
-                Title = $"{ItemController.LocalizedName(_itemData)} (T{_itemData.Tier})";
-                LblItemName.Content = $"{ItemController.LocalizedName(_itemData)} (T{_itemData.Tier})";
-                LblItemId.Content = _itemData.UniqueName;
-                ImgItemImage.Source = item.Icon;
-
-                FaLoadIcon.Visibility = Visibility.Hidden;
-            });
-        
-        }
-
-        private async void StartAutoUpdater()
-        {
-            await Task.Run(async () => {
-                if (_isAutoUpdateActive)
-                    return;
-
-                _isAutoUpdateActive = true;
-                while (_runUpdate)
-                {
-                    await Task.Delay(500);
-                    if (Dispatcher != null && Dispatcher.Invoke(() => !ChbAutoUpdateData.IsChecked ?? false))
-                        continue;
-
-                    GetPriceStats(_uniqueName);
-                    await Task.Delay(Settings.Default.RefreshRate - 500);
-                }
-                _isAutoUpdateActive = false;
-            });
-        }
-        
-        private async void GetPriceStats(string uniqueName)
-        {
-            if (uniqueName == null)
-                return;
-
-            await Task.Run(async () =>
-            {
-                var showVillagesIsChecked = Dispatcher != null && Dispatcher.Invoke(() => ChbShowVillages.IsChecked ?? false);
-                var showBlackZoneOutpostsIsChecked = Dispatcher != null && Dispatcher.Invoke(() => ChbShowBlackZoneOutposts.IsChecked ?? false);
-
-                var statPricesList = await ApiController.GetCityItemPricesFromJsonAsync(uniqueName, Locations.GetLocationsListByArea(new IsLocationAreaActive()
-                {
-                    Cities = true,
-                    Villages = showVillagesIsChecked,
-                    BlackZoneOutposts = showBlackZoneOutpostsIsChecked
-                }));
-                
-                if (statPricesList == null)
-                    return;
-
-                var statsPricesTotalList = PriceUpdate(statPricesList);
-
-                FindBestPrice(ref statsPricesTotalList);
-
-                var marketCurrentPricesItemList = new List<MarketCurrentPricesItem>();
-                foreach (var item in statsPricesTotalList)
-                    marketCurrentPricesItemList.Add(new MarketCurrentPricesItem(item));
-
-                Dispatcher?.Invoke(() =>
-                {
-                    ListViewPrices.ItemsSource = marketCurrentPricesItemList;
-                    SetDifferenceCalculationText(statsPricesTotalList);
-                    LblLastUpdate.Content = Utilities.DateFormat(DateTime.Now, 0);
-                });
-            });
-        }
-
-        private List<MarketResponseTotal> PriceUpdate(List<MarketResponse> statPricesList)
-        {
-            var statsPricesTotalList = new List<MarketResponseTotal>();
-
-            foreach (var stats in statPricesList)
-            {
-                if (statsPricesTotalList.Exists(s => Locations.GetParameterName(s.City) == stats.City))
-                {
-                    var spt = statsPricesTotalList.Find(s => Locations.GetName(s.City) == stats.City);
-                    if (stats.SellPriceMin < spt.SellPriceMin)
-                        spt.SellPriceMin = stats.SellPriceMin;
-
-                    if (stats.SellPriceMax > spt.SellPriceMax)
-                        spt.SellPriceMax = stats.SellPriceMax;
-
-                    if (stats.BuyPriceMin < spt.BuyPriceMin)
-                        spt.BuyPriceMin = stats.BuyPriceMin;
-
-                    if (stats.BuyPriceMax > spt.BuyPriceMax)
-                        spt.BuyPriceMax = stats.BuyPriceMax;
-                }
-                else
-                {
-                    statsPricesTotalList.Add(new MarketResponseTotal(stats));
-                }
-            }
-
-            return statsPricesTotalList;
-        }
-        
-        private void FindBestPrice(ref List<MarketResponseTotal> list)
-        {
-            if (list.Count == 0)
-                return;
-
-            var max = GetMaxPrice(list);
-
-            try
-            {
-                list.Find(s => s.BuyPriceMax == max).BestBuyMaxPrice = true;
-            }
-            catch (Exception ex)
-            {
-                Debug.Print(ex.ToString());
-            }
-
-            var min = GetMinPrice(list);
-
-            try
-            {
-                list.Find(s => s.SellPriceMin == min).BestSellMinPrice = true;
-            }
-            catch (Exception ex)
-            {
-                Debug.Print(ex.ToString());
-            }
-
-        }
-
-        private static ulong GetMaxPrice(List<MarketResponseTotal> list)
-        {
-            var max = ulong.MinValue;
-            foreach (var type in list)
-            {
-                if (type.BuyPriceMax == 0)
-                    continue;
-
-                if (type.BuyPriceMax > max)
-                    max = type.BuyPriceMax;
-            }
-
-            return max;
-        }
-
-        private static ulong GetMinPrice(List<MarketResponseTotal> list)
-        {
-            var min = ulong.MaxValue;
-            foreach (var type in list)
-            {
-                if (type.SellPriceMin == 0)
-                    continue;
-
-                if (type.SellPriceMin < min)
-                    min = type.SellPriceMin;
-            }
-
-            return min;
-        }
-
-        private void SetDifferenceCalculationText(List<MarketResponseTotal> statsPricesTotalList)
-        {
-            ulong? bestBuyMaxPrice = 0UL;
-            ulong? bestSellMinPrice = 0UL;
-
-            if (statsPricesTotalList?.Count > 0)
-            {
-                bestBuyMaxPrice = statsPricesTotalList.FirstOrDefault(s => s.BestBuyMaxPrice)?.BuyPriceMax ?? 0UL;
-                bestSellMinPrice = statsPricesTotalList.FirstOrDefault(s => s.BestSellMinPrice)?.SellPriceMin ?? 0UL;
-            }
-
-            var diffPrice = (int)bestBuyMaxPrice - (int)bestSellMinPrice;
-
-            LblDifCalcText.Content = $"{LanguageController.Translation("BOUGHT_FOR")} {string.Format(LanguageController.DefaultCultureInfo, "{0:n0}", bestSellMinPrice)} | " +
-                                     $"{LanguageController.Translation("SELL_FOR")} {string.Format(LanguageController.DefaultCultureInfo, "{0:n0}", bestBuyMaxPrice)} | " +
-                                     $"{LanguageController.Translation("PROFIT")} {string.Format(LanguageController.DefaultCultureInfo, "{0:n0}", diffPrice)}";
-        }
-        
         private void Hotbar_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if (e.ChangedButton == MouseButton.Left)
@@ -276,15 +29,15 @@
 
         private void CloseButton_Click(object sender, RoutedEventArgs e)
         {
-            _runUpdate = false;
+            _itemWindowViewModel.RunUpdate = false;
             Close();
         }
 
         private void MinimizeButton_Click(object sender, RoutedEventArgs e) => WindowState = WindowState.Minimized;
 
-        private void ShowVillagesPrices_Click(object sender, RoutedEventArgs e) => GetPriceStats(_uniqueName);
+        private void ShowVillagesPrices_Click(object sender, RoutedEventArgs e) => _itemWindowViewModel.GetPriceStats();
 
-        private void ChbShowBlackZoneOutposts_Click(object sender, RoutedEventArgs e) => GetPriceStats(_uniqueName);
+        private void ChbShowBlackZoneOutposts_Click(object sender, RoutedEventArgs e) => _itemWindowViewModel.GetPriceStats();
 
         private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
         {
