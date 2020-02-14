335d961d5e64bf7d59a6134d873dd94060156e70
Aaron Schultz <a.schultz@tech2go.de>
2020-02-14 13:55:38 +0100

    Add tab control + new api query with models

---
+++ StatisticsAnalysisTool/StatisticsAnalysisTool/Models/MarketHistoriesResponse.cs
@@ -0,0 +1,19 @@
+﻿namespace StatisticsAnalysisTool.Models
+{
+    using Newtonsoft.Json;
+
+    public class MarketHistoriesResponse
+    {
+        [JsonProperty(PropertyName = "location")]
+        public string Location { get; set; }
+
+        [JsonProperty(PropertyName = "itemTypeId")]
+        public string ItemTypeId { get; set; }
+
+        [JsonProperty(PropertyName = "qualityLevel")]
+        public int QualityLevel { get; set; }
+
+        [JsonProperty(PropertyName = "data")]
+        public MarketHistoryResponse Data { get; set; }
+    }
+}
---
+++ StatisticsAnalysisTool/StatisticsAnalysisTool/Models/MarketHistoryResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace StatisticsAnalysisTool.Models
+{
+    using Newtonsoft.Json;
+    using System;
+
+    public class MarketHistoryResponse
+    {
+        [JsonProperty(PropertyName = "itemCount")]
+        public int ItemCount { get; set; }
+
+        [JsonProperty(PropertyName = "averagePrice")]
+        public int AveragePrice { get; set; }
+
+        [JsonProperty(PropertyName = "timestamp")]
+        public DateTime Timestamp { get; set; }
+    }
+}
