3374e51b080f713521728780287d8224e36d0f60
Aaron Schultz <schultzaaron@web.de>
2020-03-03 09:09:37 +0100

    Add info button

--- StatisticsAnalysisTool/StatisticsAnalysisTool/Views/InfoWindow.xaml.cs
+++ StatisticsAnalysisTool/StatisticsAnalysisTool/Views/InfoWindow.xaml.cs
@@ -1,27 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace StatisticsAnalysisTool.Views
 {
     /// <summary>
     /// Interaction logic for InfoWindow.xaml
     /// </summary>
-    public partial class InfoWindow : Window
+    public partial class InfoWindow
     {
         public InfoWindow()
         {
             InitializeComponent();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e) => Close();
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e) => WindowState = WindowState.Minimized;
+
+        private void Hotbar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                DragMove();
+        }
+
+        private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2 && WindowState == WindowState.Normal)
+            {
+                WindowState = WindowState.Maximized;
+                return;
+            }
+
+            if (e.ClickCount == 2 && WindowState == WindowState.Maximized) WindowState = WindowState.Normal;
         }
     }
 }
