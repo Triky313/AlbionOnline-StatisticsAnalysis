993f35d9ca6e0c900073f43235071cd39ecfa2ae
Aaron Schultz <schultzaaron@web.de>
2020-06-06 09:09:19 +0200

    Added new models

---
+++ StatisticsAnalysisTool/StatisticsAnalysisTool/Models/WeaponCategories.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace StatisticsAnalysisTool.Models
+{
+    public class WeaponCategories
+    {
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+
+}
