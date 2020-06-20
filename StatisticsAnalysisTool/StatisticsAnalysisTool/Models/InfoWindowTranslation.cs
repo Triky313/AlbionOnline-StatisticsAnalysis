83262dfeff4d2e5929898951d8640dfeb5484dea
Aaron Schultz <schultzaaron@web.de>
2020-06-20 19:49:27 +0200

    Change InfoWindow

---
+++ StatisticsAnalysisTool/StatisticsAnalysisTool/Models/InfoWindowTranslation.cs
@@ -0,0 +1,11 @@
+﻿using StatisticsAnalysisTool.Common;
+
+namespace StatisticsAnalysisTool.Models
+{
+    public class InfoWindowTranslation
+    {
+        public string ShowNotAgainContent => LanguageController.Translation("SHOW_NOT_AGAIN");
+        public string Title => LanguageController.Translation("NEW_FEATURE");
+        public string FeatureDescription => LanguageController.Translation("FEATURE_DESCRIPTION");
+    }
+}
