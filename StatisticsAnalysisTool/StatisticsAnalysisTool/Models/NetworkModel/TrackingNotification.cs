6eaf094148057db064b37c766bc32eebf0c53876
Aaron Schultz <schultzaaron@web.de>
2021-01-09 15:41:51 +0100

    Added TrackingNotification

---
+++ StatisticsAnalysisTool/StatisticsAnalysisTool/Models/NetworkModel/TrackingNotification.cs
@@ -0,0 +1,12 @@
+﻿namespace StatisticsAnalysisTool.Models.NetworkModel
+{
+    public class TrackingNotification
+    {
+        public TrackingNotification(string text)
+        {
+            Text = text;
+        }
+
+        public string Text { get; set; }
+    }
+}
