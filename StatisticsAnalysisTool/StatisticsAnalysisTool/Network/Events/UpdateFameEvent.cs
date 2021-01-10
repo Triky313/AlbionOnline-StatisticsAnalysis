8ccdc7f3ac06c9fccbc0d63a832bed2bde4dde08
Aaron Schultz <schultzaaron@web.de>
2021-01-10 14:21:48 +0100

    Added TotalGainedFame to fame update event

--- StatisticsAnalysisTool/StatisticsAnalysisTool/Network/Events/UpdateFameEvent.cs
+++ StatisticsAnalysisTool/StatisticsAnalysisTool/Network/Events/UpdateFameEvent.cs
@@ -7,6 +7,8 @@ namespace StatisticsAnalysisTool.Network.Handler
 {
     public class UpdateFameEvent : BaseEvent
     {
+        private readonly double _fameWithZoneAndPremium = 0;
+
         public UpdateFameEvent(Dictionary<byte, object> parameters) : base(parameters)
         {
             try
@@ -18,7 +20,7 @@ namespace StatisticsAnalysisTool.Network.Handler
 
                 if (parameters.ContainsKey(1) && long.TryParse(parameters[1].ToString(), out long totalFame))
                 {
-                    TotalFame = totalFame / 10000d;
+                    TotalPlayerFame = totalFame / 10000d;
                 }
 
                 if (parameters.ContainsKey(2) && long.TryParse(parameters[2].ToString(), out var fameWithZoneAndWithoutPremiumUnrounded))
@@ -39,15 +41,16 @@ namespace StatisticsAnalysisTool.Network.Handler
                     }
                 }
 
+                SatchelFame = 0;
                 if (parameters.ContainsKey(9) && long.TryParse(parameters[9].ToString(), out var satchelFameUnrounded))
                 {
-                    SatchelFame = 0;
                     if (satchelFameUnrounded >= 1)
                     {
                         SatchelFame = satchelFameUnrounded / 10000d;
                     }
                 }
 
+                NormalFame = 0;
                 if (FameWithZoneAndWithoutPremium > 0 && ZoneMultiplier >= 1)
                 {
                     NormalFame = FameWithZoneAndWithoutPremium / ZoneMultiplier;
@@ -55,18 +58,22 @@ namespace StatisticsAnalysisTool.Network.Handler
 
                 if (FameWithZoneAndWithoutPremium > 0)
                 {
-                    FameWithZoneAndPremium = FameWithZoneAndWithoutPremium * 1.5f;
+                    _fameWithZoneAndPremium = FameWithZoneAndWithoutPremium * 1.5f;
                 }
 
-                if (FameWithZoneAndPremium > 0 && FameWithZoneAndWithoutPremium > 0)
+                PremiumFame = 0;
+                if (_fameWithZoneAndPremium > 0 && FameWithZoneAndWithoutPremium > 0)
                 {
-                    PremiumFame = FameWithZoneAndPremium - FameWithZoneAndWithoutPremium;
+                    PremiumFame = _fameWithZoneAndPremium - FameWithZoneAndWithoutPremium;
                 }
 
+                ZoneFame = 0;
                 if (FameWithZoneAndWithoutPremium >= NormalFame)
                 {
                     ZoneFame = FameWithZoneAndWithoutPremium - NormalFame;
                 }
+
+                TotalGainedFame = NormalFame + ZoneFame + PremiumFame + SatchelFame;
             }
             catch (Exception e)
             {
@@ -74,13 +81,13 @@ namespace StatisticsAnalysisTool.Network.Handler
             }
         }
 
-        public double TotalFame { get; }
+        public double TotalPlayerFame { get; }
         public double FameWithZoneAndWithoutPremium { get; }
         public double ZoneMultiplier { get; }
         public double NormalFame { get; }
-        public double FameWithZoneAndPremium { get; }
         public double PremiumFame { get; }
         public double ZoneFame { get; }
         public double SatchelFame { get; }
+        public double TotalGainedFame { get; }
     }
 }
