8c8e25af82de5791cc71316926f6e1843ff0b11e
Aaron Schultz <schultzaaron@web.de>
2021-01-24 18:01:55 +0100

    Add new item price dateTime format

--- StatisticsAnalysisTool/StatisticsAnalysisTool/Common/Formatting.cs
+++ StatisticsAnalysisTool/StatisticsAnalysisTool/Common/Formatting.cs
@@ -8,6 +8,32 @@ namespace StatisticsAnalysisTool.Common
     {
         public static string CurrentDateTimeFormat(DateTime value) => DateTime.SpecifyKind(value, DateTimeKind.Utc).ToLocalTime().ToString("G", new CultureInfo(LanguageController.CurrentCultureInfo.TextInfo.CultureName));
 
+        public static string DateTimeToLastUpdateTime(DateTime dateTime)
+        {
+            var startTime = dateTime;
+            var endTime = DateTime.UtcNow;
+            var minutes = (endTime - startTime).TotalMinutes;
+            var hours = (endTime - startTime).TotalHours;
+            var days = (endTime - startTime).TotalDays;
+
+            if (minutes <= 120)
+            {
+                return $"{minutes:N0} {LanguageController.Translation("MINUTES")}";
+            }
+
+            if (hours <= 48)
+            {
+                return $"{hours:N0} {LanguageController.Translation("HOURS")}";
+            }
+
+            if (days <= 365)
+            {
+                return $"{days:N0} {LanguageController.Translation("DAYS")}";
+            }
+
+            return $"{LanguageController.Translation("OVER_A_YEAR")}";
+        }
+
         public static string ToStringShort(double num)
         {
             if (num < -10000000)
