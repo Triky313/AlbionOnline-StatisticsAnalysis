1bb8deb446db2cc17ca74ef0da7f9a0e4286fe53
Aaron Schultz <schultzaaron@web.de>
2021-03-28 19:24:48 +0200

    Fix a respec points tracking exception

--- StatisticsAnalysisTool/StatisticsAnalysisTool/Network/Events/UpdateReSpecPointsEvent.cs
+++ StatisticsAnalysisTool/StatisticsAnalysisTool/Network/Events/UpdateReSpecPointsEvent.cs
@@ -1,16 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Reflection;
-using Albion.Network;
+﻿using Albion.Network;
 using log4net;
 using PcapDotNet.Base;
 using StatisticsAnalysisTool.Common;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
 
 namespace StatisticsAnalysisTool.Network.Events
 {
     public class UpdateReSpecPointsEvent : BaseEvent
     {
+        public FixPoint? CurrentReSpecPoints { get; }
+
         private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
 
         public UpdateReSpecPointsEvent(Dictionary<byte, object> parameters) : base(parameters)
@@ -19,11 +21,26 @@ namespace StatisticsAnalysisTool.Network.Events
             {
                 if (parameters.ContainsKey(0) && parameters[0] != null)
                 {
-                    var reSpecPointsArray = ((long[]) parameters[0]).ToDictionary();
+                    var parameterType = parameters[0].GetType();
 
-                    if (!reSpecPointsArray.IsNullOrEmpty() && reSpecPointsArray.ContainsKey(1) &&
-                        long.TryParse(reSpecPointsArray[1].ToString(), out var currentReSpecPoints))
-                        CurrentReSpecPoints = currentReSpecPoints / 10000d;
+                    if(parameterType.Name == "Int32[]") 
+                    {
+                        var reSpecPointsArray = ((int[])parameters[0]).ToDictionary();
+
+                        if (!reSpecPointsArray.IsNullOrEmpty() && reSpecPointsArray.ContainsKey(1))
+                        {
+                            CurrentReSpecPoints = FixPoint.FromInternalValue(reSpecPointsArray[1].ObjectToLong() ?? 0);
+                        }
+                    } 
+                    else if(parameterType.Name == "Int64[]") 
+                    {
+                        var reSpecPointsArray = ((long[])parameters[0]).ToDictionary();
+
+                        if (!reSpecPointsArray.IsNullOrEmpty() && reSpecPointsArray.ContainsKey(1))
+                        {
+                            CurrentReSpecPoints = FixPoint.FromInternalValue(reSpecPointsArray[1].ObjectToLong() ?? 0);
+                        }
+                    }
                 }
             }
             catch (ArgumentNullException e)
@@ -37,7 +54,5 @@ namespace StatisticsAnalysisTool.Network.Events
                 Debug.Print(e.Message);
             }
         }
-
-        public double CurrentReSpecPoints { get; }
     }
 }
