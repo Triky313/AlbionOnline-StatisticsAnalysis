4670fa88c7590031cf4074a89bb60f2c46b5d4e5
Aaron Schultz <schultzaaron@web.de>
2021-06-06 15:28:09 +0200

    Faction points are displayed correctly again

--- StatisticsAnalysisTool/StatisticsAnalysisTool/Common/FactionWarfareController.cs
+++ StatisticsAnalysisTool/StatisticsAnalysisTool/Common/FactionWarfareController.cs
@@ -34,9 +34,9 @@ namespace StatisticsAnalysisTool.Common
                 case 5:
                     return CityFaction.Thetford;
                 case 4:
-                    return CityFaction.Bridgewatch;
-                case 3:
                     return CityFaction.FortSterling;
+                case 3:
+                    return CityFaction.Bridgewatch;
                 case 2:
                     return CityFaction.Lymhurst;
                 case 1:
--- StatisticsAnalysisTool/StatisticsAnalysisTool/Network/Events/UpdateCurrencyEvent.cs
+++ StatisticsAnalysisTool/StatisticsAnalysisTool/Network/Events/UpdateCurrencyEvent.cs
@@ -12,32 +12,45 @@ namespace StatisticsAnalysisTool.Network.Handler
         public CityFaction CityFaction;
         public FixPoint GainedFactionCoins;
         public FixPoint BonusPremiumGainedFractionFlagPoints;
+        public FixPoint FactionRankPoints;
+        public FixPoint FactionRankPointsPopulationBonus;
+        public FixPoint FactionRankPointsPremiumBonus;
         public FixPoint TotalPlayerFactionPoints;
 
         public UpdateCurrencyEvent(Dictionary<byte, object> parameters) : base(parameters)
         {
             ConsoleManager.WriteLineForNetworkHandler(GetType().Name, parameters);
-
+            
             try
             {
-                if (parameters.ContainsKey(1))
-                {
-                    CityFaction = FactionWarfareController.GetCityFactionType(parameters[1].ObjectToByte());
-                }
-
                 if (parameters.ContainsKey(2))
                 {
-                    GainedFactionCoins = FixPoint.FromInternalValue(parameters[2].ObjectToLong() ?? 0);
+                    CityFaction = FactionWarfareController.GetCityFactionType(parameters[2].ObjectToByte());
                 }
 
-                //if (parameters.ContainsKey(2))
-                //{
-                //    BonusPremiumGainedFractionFlagPoints = FixPoint.FromInternalValue(parameters[2].ObjectToLong() ?? 0);
-                //}
+                if (parameters.ContainsKey(3))
+                {
+                    GainedFactionCoins = FixPoint.FromInternalValue(parameters[3].ObjectToLong() ?? 0);
+                }
+
+                if (parameters.ContainsKey(4))
+                {
+                    FactionRankPoints = FixPoint.FromInternalValue(parameters[4].ObjectToLong() ?? 0);
+                }
+
+                if (parameters.ContainsKey(6))
+                {
+                    FactionRankPointsPopulationBonus = FixPoint.FromInternalValue(parameters[6].ObjectToLong() ?? 0);
+                }
 
                 if (parameters.ContainsKey(8))
                 {
-                    TotalPlayerFactionPoints = FixPoint.FromInternalValue(parameters[8].ObjectToLong() ?? 0);
+                    FactionRankPointsPremiumBonus = FixPoint.FromInternalValue(parameters[8].ObjectToLong() ?? 0);
+                }
+
+                if (parameters.ContainsKey(9))
+                {
+                    TotalPlayerFactionPoints = FixPoint.FromInternalValue(parameters[9].ObjectToLong() ?? 0);
                 }
             }
             catch (Exception e)
