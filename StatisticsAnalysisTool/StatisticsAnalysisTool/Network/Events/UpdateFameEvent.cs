264e607acc2ed414edaf8f8ba0a874b273f20f7f
Aaron Schultz <schultzaaron@web.de>
2021-07-19 17:45:18 +0200

    Fix fame calculation with fame event bonus

--- StatisticsAnalysisTool/StatisticsAnalysisTool/Network/Events/UpdateFameEvent.cs
+++ StatisticsAnalysisTool/StatisticsAnalysisTool/Network/Events/UpdateFameEvent.cs
@@ -9,11 +9,19 @@ namespace StatisticsAnalysisTool.Network.Handler
     public class UpdateFameEvent : BaseEvent
     {
         public float BonusFactor;
-        public FixPoint Change; // Fame with Zone Multiplier and without Premium
+        public float BonusFactorInPercent;
+        public FixPoint FameWithZoneMultiplier; // Fame with Zone Multiplier and without Premium
         public byte GroupSize;
         public bool IsPremiumBonus;
         public FixPoint SatchelFame;
         public int UsedItemType;
+
+        public FixPoint TotalPlayerFame { get; }
+        public FixPoint Multiplier { get; }
+        public FixPoint NormalFame { get; }
+        public FixPoint PremiumFame { get; }
+        public FixPoint ZoneFame { get; }
+        public FixPoint TotalGainedFame { get; }
 
         public UpdateFameEvent(Dictionary<byte, object> parameters) : base(parameters)
         {
@@ -34,58 +42,65 @@ namespace StatisticsAnalysisTool.Network.Handler
                     TotalPlayerFame = FixPoint.FromInternalValue(totalPlayerFame);
                 }
 
-                if (parameters.ContainsKey(2)) Change = FixPoint.FromInternalValue(parameters[2].ObjectToLong() ?? 0);
+                if (parameters.ContainsKey(3)) 
+                    GroupSize = parameters[3] as byte? ?? 0;
 
-                if (parameters.ContainsKey(3)) GroupSize = parameters[3] as byte? ?? 0;
+                if (parameters.ContainsKey(4)) 
+                    Multiplier = FixPoint.FromInternalValue(parameters[4].ObjectToLong() ?? 0);
 
-                if (parameters.ContainsKey(4)) Multiplier = FixPoint.FromInternalValue(parameters[4].ObjectToLong() ?? 0);
+                if (parameters.ContainsKey(5)) 
+                    IsPremiumBonus = parameters[5] as bool? ?? false;
 
-                if (parameters.ContainsKey(5)) IsPremiumBonus = parameters[5] as bool? ?? false;
-
-                if (parameters.ContainsKey(6)) BonusFactor = parameters[6] as float? ?? 0;
-
-                if (parameters.ContainsKey(9)) SatchelFame = FixPoint.FromInternalValue(parameters[9].ObjectToLong() ?? 0);
-
-                if (parameters.ContainsKey(252)) UsedItemType = parameters[252].ObjectToInt();
-
-                if (Change.DoubleValue > 0 && Multiplier.DoubleValue > 0)
+                if (parameters.ContainsKey(6))
                 {
-                    var newNormalFame = Change.DoubleValue / Multiplier.DoubleValue;
+                    BonusFactor = parameters[6] as float? ?? 1;
+                    BonusFactorInPercent = (BonusFactor - 1) * 100;
+                }
+
+                if (parameters.ContainsKey(2))
+                {
+                    var fameWithZoneMultiplier = FixPoint.FromInternalValue(parameters[2].ObjectToLong() ?? 0);
+                    FameWithZoneMultiplier = FixPoint.FromFloatingPointValue(fameWithZoneMultiplier.DoubleValue * BonusFactor);
+                }
+
+                if (parameters.ContainsKey(9)) 
+                    SatchelFame = FixPoint.FromInternalValue(parameters[9].ObjectToLong() ?? 0);
+
+                if (parameters.ContainsKey(252)) 
+                    UsedItemType = parameters[252].ObjectToInt();
+
+                if (FameWithZoneMultiplier.DoubleValue > 0 && Multiplier.DoubleValue > 0)
+                {
+                    var newNormalFame = FameWithZoneMultiplier.DoubleValue / Multiplier.DoubleValue * BonusFactor;
                     NormalFame = FixPoint.FromFloatingPointValue(newNormalFame);
                 }
 
                 double fameWithZoneAndPremium = 0;
-                if (Change.DoubleValue > 0)
+                if (FameWithZoneMultiplier.DoubleValue > 0)
                 {
                     if (IsPremiumBonus)
-                        fameWithZoneAndPremium = Change.DoubleValue * 1.5f;
+                        fameWithZoneAndPremium = FameWithZoneMultiplier.DoubleValue * 1.5f;
                     else
-                        fameWithZoneAndPremium = Change.DoubleValue;
+                        fameWithZoneAndPremium = FameWithZoneMultiplier.DoubleValue;
                 }
 
-                if (fameWithZoneAndPremium > 0 && Change.DoubleValue > 0)
+                if (fameWithZoneAndPremium > 0 && FameWithZoneMultiplier.DoubleValue > 0)
                 {
-                    var newPremiumFame = fameWithZoneAndPremium - Change.DoubleValue;
+                    var newPremiumFame = fameWithZoneAndPremium - FameWithZoneMultiplier.DoubleValue;
                     PremiumFame = FixPoint.FromFloatingPointValue(newPremiumFame);
                 }
 
-                if (Change.InternalValue >= NormalFame.InternalValue)
-                    ZoneFame = FixPoint.FromInternalValue(Change.InternalValue - NormalFame.InternalValue);
+                if (FameWithZoneMultiplier.InternalValue >= NormalFame.InternalValue)
+                {
+                    ZoneFame = FixPoint.FromFloatingPointValue(FameWithZoneMultiplier.DoubleValue - (FameWithZoneMultiplier.DoubleValue / Multiplier.DoubleValue));
+                }
 
-                TotalGainedFame = NormalFame + ZoneFame + PremiumFame + SatchelFame;
+                TotalGainedFame = FameWithZoneMultiplier + PremiumFame + SatchelFame;
             }
             catch (Exception e)
             {
                 ConsoleManager.WriteLineForError(MethodBase.GetCurrentMethod().DeclaringType, e);
             }
         }
-
-        public FixPoint TotalPlayerFame { get; }
-        public FixPoint Multiplier { get; }
-
-        public FixPoint NormalFame { get; }
-        public FixPoint PremiumFame { get; }
-        public FixPoint ZoneFame { get; }
-        public FixPoint TotalGainedFame { get; }
     }
 }
