eb6e1291e0ac151d7cf1ed9e3f35051bdfb8d5c3
Aaron Schultz <schultzaaron@web.de>
2021-08-15 10:01:28 +0200

    Cleanup api controller + higher timeouts

--- StatisticsAnalysisTool/StatisticsAnalysisTool/Common/ApiController.cs
+++ StatisticsAnalysisTool/StatisticsAnalysisTool/Common/ApiController.cs
@@ -21,51 +21,46 @@ namespace StatisticsAnalysisTool.Common
         {
             var url = $"https://gameinfo.albiononline.com/api/gameinfo/items/{uniqueName}/data";
 
-            using (var client = new HttpClient())
+            using var client = new HttpClient();
+            client.Timeout = TimeSpan.FromSeconds(120);
+
+            try
             {
-                client.Timeout = TimeSpan.FromSeconds(60);
-                try
+                using var response = await client.GetAsync(url);
+                using var content = response.Content;
+                var emptyItemInfo = new ItemInformation
                 {
-                    using (var response = await client.GetAsync(url))
-                    {
-                        using (var content = response.Content)
-                        {
-                            var emptyItemInfo = new ItemInformation
-                            {
-                                UniqueName = uniqueName,
-                                LastUpdate = DateTime.Now
-                            };
+                    UniqueName = uniqueName,
+                    LastUpdate = DateTime.Now
+                };
 
-                            if (response.StatusCode == HttpStatusCode.NotFound)
-                            {
-                                emptyItemInfo.HttpStatus = HttpStatusCode.NotFound;
-                                return emptyItemInfo;
-                            }
-
-                            if (response.IsSuccessStatusCode)
-                            {
-                                var itemInfo = JsonConvert.DeserializeObject<ItemInformation>(await content.ReadAsStringAsync());
-                                itemInfo.HttpStatus = HttpStatusCode.OK;
-                                return itemInfo;
-                            }
-
-                            emptyItemInfo.HttpStatus = response.StatusCode;
-                            return emptyItemInfo;
-                        }
-                    }
-                }
-                catch (TaskCanceledException ex)
+                if (response.StatusCode == HttpStatusCode.NotFound)
                 {
-                    ConsoleManager.WriteLineForError(MethodBase.GetCurrentMethod().DeclaringType, ex);
-                    Log.Error(MethodBase.GetCurrentMethod().DeclaringType, ex);
-                    return null;
+                    emptyItemInfo.HttpStatus = HttpStatusCode.NotFound;
+                    return emptyItemInfo;
                 }
-                catch (Exception e)
+
+                if (response.IsSuccessStatusCode)
                 {
-                    ConsoleManager.WriteLineForError(MethodBase.GetCurrentMethod().DeclaringType, e);
-                    Log.Error(MethodBase.GetCurrentMethod().DeclaringType, e);
-                    return null;
+                    var itemInfo = JsonConvert.DeserializeObject<ItemInformation>(await content.ReadAsStringAsync());
+                    itemInfo.HttpStatus = HttpStatusCode.OK;
+                    return itemInfo;
                 }
+
+                emptyItemInfo.HttpStatus = response.StatusCode;
+                return emptyItemInfo;
+            }
+            catch (TaskCanceledException ex)
+            {
+                ConsoleManager.WriteLineForError(MethodBase.GetCurrentMethod().DeclaringType, ex);
+                Log.Error(MethodBase.GetCurrentMethod().DeclaringType, ex);
+                return null;
+            }
+            catch (Exception e)
+            {
+                ConsoleManager.WriteLineForError(MethodBase.GetCurrentMethod().DeclaringType, e);
+                Log.Error(MethodBase.GetCurrentMethod().DeclaringType, e);
+                return null;
             }
         }
 
@@ -105,36 +100,30 @@ namespace StatisticsAnalysisTool.Common
                 url = qualities.Aggregate(url, (current, quality) => current + $"{quality},");
             }
 
-            using (var client = new HttpClient())
+            using var client = new HttpClient();
+            try
             {
-                try
-                {
-                    client.Timeout = TimeSpan.FromSeconds(30);
+                client.Timeout = TimeSpan.FromSeconds(30);
 
-                    using (var response = await client.GetAsync(url))
-                    {
-                        if (response.StatusCode == (HttpStatusCode) 429)
-                        {
-                            throw new TooManyRequestsException();
-                        }
-
-                        using (var content = response.Content)
-                        {
-                            return JsonConvert.DeserializeObject<List<MarketResponse>>(await content.ReadAsStringAsync());
-                        }
-                    }
-                }
-                catch (TooManyRequestsException)
+                using var response = await client.GetAsync(url);
+                if (response.StatusCode == (HttpStatusCode) 429)
                 {
-                    ConsoleManager.WriteLineForWarning(MethodBase.GetCurrentMethod().DeclaringType, new TooManyRequestsException());
                     throw new TooManyRequestsException();
                 }
-                catch (Exception e)
-                {
-                    ConsoleManager.WriteLineForError(MethodBase.GetCurrentMethod().DeclaringType, e);
-                    Log.Error(MethodBase.GetCurrentMethod().DeclaringType, e);
-                    return null;
-                }
+
+                using var content = response.Content;
+                return JsonConvert.DeserializeObject<List<MarketResponse>>(await content.ReadAsStringAsync());
+            }
+            catch (TooManyRequestsException)
+            {
+                ConsoleManager.WriteLineForWarning(MethodBase.GetCurrentMethod().DeclaringType, new TooManyRequestsException());
+                throw new TooManyRequestsException();
+            }
+            catch (Exception e)
+            {
+                ConsoleManager.WriteLineForError(MethodBase.GetCurrentMethod().DeclaringType, e);
+                Log.Error(MethodBase.GetCurrentMethod().DeclaringType, e);
+                return null;
             }
         }
 
@@ -157,32 +146,29 @@ namespace StatisticsAnalysisTool.Common
             url += $"&qualities={qualitiesString}";
             url += $"&time-scale={timeScale}";
 
-            using (var client = new HttpClient())
+            using var client = new HttpClient();
+            client.Timeout = TimeSpan.FromSeconds(60);
+            try
             {
-                client.Timeout = TimeSpan.FromSeconds(30);
-                try
+                using var response = await client.GetAsync(url);
+                using var content = response.Content;
+                if (response.StatusCode == (HttpStatusCode) 429)
                 {
-                    using (var response = await client.GetAsync(url))
-                    {
-                        using (var content = response.Content)
-                        {
-                            if (response.StatusCode == (HttpStatusCode) 429) throw new TooManyRequestsException();
-
-                            return JsonConvert.DeserializeObject<List<MarketHistoriesResponse>>(await content.ReadAsStringAsync());
-                        }
-                    }
-                }
-                catch (TooManyRequestsException)
-                {
-                    ConsoleManager.WriteLineForWarning(MethodBase.GetCurrentMethod().DeclaringType, new TooManyRequestsException());
                     throw new TooManyRequestsException();
                 }
-                catch (Exception e)
-                {
-                    ConsoleManager.WriteLineForError(MethodBase.GetCurrentMethod().DeclaringType, e);
-                    Log.Error(MethodBase.GetCurrentMethod().DeclaringType, e);
-                    return null;
-                }
+
+                return JsonConvert.DeserializeObject<List<MarketHistoriesResponse>>(await content.ReadAsStringAsync());
+            }
+            catch (TooManyRequestsException)
+            {
+                ConsoleManager.WriteLineForWarning(MethodBase.GetCurrentMethod().DeclaringType, new TooManyRequestsException());
+                throw new TooManyRequestsException();
+            }
+            catch (Exception e)
+            {
+                ConsoleManager.WriteLineForError(MethodBase.GetCurrentMethod().DeclaringType, e);
+                Log.Error(MethodBase.GetCurrentMethod().DeclaringType, e);
+                return null;
             }
         }
 
@@ -191,25 +177,19 @@ namespace StatisticsAnalysisTool.Common
             var gameInfoSearchResponse = new GameInfoSearchResponse();
             var url = $"https://gameinfo.albiononline.com/api/gameinfo/search?q={username}";
 
-            using (var client = new HttpClient())
+            using var client = new HttpClient();
+            client.Timeout = TimeSpan.FromSeconds(120);
+            try
             {
-                client.Timeout = TimeSpan.FromSeconds(30);
-                try
-                {
-                    using (var response = await client.GetAsync(url))
-                    {
-                        using (var content = response.Content)
-                        {
-                            return JsonConvert.DeserializeObject<GameInfoSearchResponse>(await content.ReadAsStringAsync()) ?? gameInfoSearchResponse;
-                        }
-                    }
-                }
-                catch (Exception e)
-                {
-                    ConsoleManager.WriteLineForError(MethodBase.GetCurrentMethod().DeclaringType, e);
-                    Log.Error(MethodBase.GetCurrentMethod().DeclaringType, e);
-                    return gameInfoSearchResponse;
-                }
+                using var response = await client.GetAsync(url);
+                using var content = response.Content;
+                return JsonConvert.DeserializeObject<GameInfoSearchResponse>(await content.ReadAsStringAsync()) ?? gameInfoSearchResponse;
+            }
+            catch (Exception e)
+            {
+                ConsoleManager.WriteLineForError(MethodBase.GetCurrentMethod().DeclaringType, e);
+                Log.Error(MethodBase.GetCurrentMethod().DeclaringType, e);
+                return gameInfoSearchResponse;
             }
         }
 
@@ -218,80 +198,68 @@ namespace StatisticsAnalysisTool.Common
             var gameInfoPlayerResponse = new GameInfoPlayersResponse();
             var url = $"https://gameinfo.albiononline.com/api/gameinfo/players/{userid}";
 
-            using (var client = new HttpClient())
+            using var client = new HttpClient();
+            client.Timeout = TimeSpan.FromSeconds(120);
+            try
             {
-                client.Timeout = TimeSpan.FromSeconds(30);
-                try
-                {
-                    using (var response = await client.GetAsync(url))
-                    {
-                        using (var content = response.Content)
-                        {
-                            return JsonConvert.DeserializeObject<GameInfoPlayersResponse>(await content.ReadAsStringAsync()) ??
-                                   gameInfoPlayerResponse;
-                        }
-                    }
-                }
-                catch (Exception e)
-                {
-                    ConsoleManager.WriteLineForError(MethodBase.GetCurrentMethod().DeclaringType, e);
-                    Log.Error(MethodBase.GetCurrentMethod().DeclaringType, e);
-                    return gameInfoPlayerResponse;
-                }
+                using var response = await client.GetAsync(url);
+                using var content = response.Content;
+                return JsonConvert.DeserializeObject<GameInfoPlayersResponse>(await content.ReadAsStringAsync()) ??
+                       gameInfoPlayerResponse;
+            }
+            catch (Exception e)
+            {
+                ConsoleManager.WriteLineForError(MethodBase.GetCurrentMethod().DeclaringType, e);
+                Log.Error(MethodBase.GetCurrentMethod().DeclaringType, e);
+                return gameInfoPlayerResponse;
             }
         }
 
-        public static async Task<GameInfoGuildsResponse> GetGameInfoGuildsFromJsonAsync(string guildId)
-        {
-            var url = $"https://gameinfo.albiononline.com/api/gameinfo/guilds/{guildId}";
+        //public static async Task<GameInfoGuildsResponse> GetGameInfoGuildsFromJsonAsync(string guildId)
+        //{
+        //    var url = $"https://gameinfo.albiononline.com/api/gameinfo/guilds/{guildId}";
 
-            using (var client = new HttpClient())
-            {
-                client.Timeout = TimeSpan.FromSeconds(30);
-                try
-                {
-                    using (var response = await client.GetAsync(url))
-                    {
-                        using (var content = response.Content)
-                        {
-                            return JsonConvert.DeserializeObject<GameInfoGuildsResponse>(await content.ReadAsStringAsync());
-                        }
-                    }
-                }
-                catch (Exception e)
-                {
-                    ConsoleManager.WriteLineForError(MethodBase.GetCurrentMethod().DeclaringType, e);
-                    Log.Error(MethodBase.GetCurrentMethod().DeclaringType, e);
-                    return null;
-                }
-            }
-        }
+        //    using (var client = new HttpClient())
+        //    {
+        //        client.Timeout = TimeSpan.FromSeconds(30);
+        //        try
+        //        {
+        //            using (var response = await client.GetAsync(url))
+        //            {
+        //                using (var content = response.Content)
+        //                {
+        //                    return JsonConvert.DeserializeObject<GameInfoGuildsResponse>(await content.ReadAsStringAsync());
+        //                }
+        //            }
+        //        }
+        //        catch (Exception e)
+        //        {
+        //            ConsoleManager.WriteLineForError(MethodBase.GetCurrentMethod().DeclaringType, e);
+        //            Log.Error(MethodBase.GetCurrentMethod().DeclaringType, e);
+        //            return null;
+        //        }
+        //    }
+        //}
 
         public static async Task<List<GoldResponseModel>> GetGoldPricesFromJsonAsync(DateTime? dateTime, int count, int timeout = 30)
         {
             var checkedDateTime = dateTime != null ? dateTime.ToString() : string.Empty;
 
             var url = $"{Settings.Default.GoldStatsApiUrl ?? Settings.Default.GoldStatsApiUrlDefault}?date={checkedDateTime}&count={count}";
-            using (var client = new HttpClient())
+            using var client = new HttpClient();
+            client.Timeout = TimeSpan.FromSeconds(timeout);
+            try
             {
-                client.Timeout = TimeSpan.FromSeconds(timeout);
-                try
-                {
-                    using (var response = await client.GetAsync(url))
-                    {
-                        using (var content = response.Content)
-                        {
-                            var contentString = await content.ReadAsStringAsync();
-                            return string.IsNullOrEmpty(contentString) ? new List<GoldResponseModel>() : JsonConvert.DeserializeObject<List<GoldResponseModel>>(contentString);
-                        }
-                    }
-                }
-                catch (Exception e)
-                {
-                    ConsoleManager.WriteLineForError(MethodBase.GetCurrentMethod().DeclaringType, e);
-                    Log.Error(MethodBase.GetCurrentMethod().DeclaringType, e);
-                    return new List<GoldResponseModel>();
-                }
+                using var response = await client.GetAsync(url);
+                using var content = response.Content;
+                var contentString = await content.ReadAsStringAsync();
+                return string.IsNullOrEmpty(contentString) ? new List<GoldResponseModel>() : JsonConvert.DeserializeObject<List<GoldResponseModel>>(contentString);
+            }
+            catch (Exception e)
+            {
+                ConsoleManager.WriteLineForError(MethodBase.GetCurrentMethod().DeclaringType, e);
+                Log.Error(MethodBase.GetCurrentMethod().DeclaringType, e);
+                return new List<GoldResponseModel>();
             }
         }
     }
