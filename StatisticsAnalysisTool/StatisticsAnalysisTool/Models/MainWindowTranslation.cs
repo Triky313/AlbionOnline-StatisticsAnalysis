c43d4550d3c35e0cbfcaa8e448a2977915fae1b3
Aaron Schultz <schultzaaron@web.de>
2021-08-25 18:05:53 +0200

    Add tracker on/off button tooltip

--- StatisticsAnalysisTool/StatisticsAnalysisTool/Models/MainWindowTranslation.cs
+++ StatisticsAnalysisTool/StatisticsAnalysisTool/Models/MainWindowTranslation.cs
@@ -64,5 +64,6 @@ namespace StatisticsAnalysisTool.Models
         public string MapTracking => LanguageController.Translation("MAP_TRACKING");
         public string TryDownloadingTheItemListAgain => LanguageController.Translation("TRY_DOWNLOADING_THE_ITEM_LIST_AGAIN");
         public string DamageMeterOnOff => LanguageController.Translation("DAMAGE_METER_ON_OFF");
+        public string TrackerOnOff => LanguageController.Translation("TRACKER_ON_OFF");
     }
 }
