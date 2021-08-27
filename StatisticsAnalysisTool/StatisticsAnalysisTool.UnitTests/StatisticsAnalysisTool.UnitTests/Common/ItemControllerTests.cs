04d96e9da6bc76ee2fd66fadbb57cb7e1b61f9d2
Aaron Schultz <schultzaaron@web.de>
2021-08-27 16:03:02 +0200

    Neu test + nugets

---
+++ StatisticsAnalysisTool/StatisticsAnalysisTool.UnitTests/StatisticsAnalysisTool.UnitTests/Common/ItemControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using StatisticsAnalysisTool.Common;
+using StatisticsAnalysisTool.Models;
+using System.Collections.ObjectModel;
+
+namespace StatisticsAnalysisTool.UnitTests.Common
+{
+    public class ItemControllerTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void IsMob_WithValidString_ReturnTrue()
+        {
+            var receivedItem1 = new Item() { Index = 114 };
+            var receivedItem2 = new Item() { Index = 11 };
+            var receivedItem3 = new Item() { Index = 512 };
+
+            var itemList = new ObservableCollection<Item>
+            {
+                receivedItem1,
+                receivedItem2,
+                receivedItem3,
+            };
+
+            ItemController.Items = itemList;
+
+            var expectedItem = ItemController.GetItemByIndex(11);
+            Assert.Equals(receivedItem1, expectedItem);
+        }
+    }
+}
