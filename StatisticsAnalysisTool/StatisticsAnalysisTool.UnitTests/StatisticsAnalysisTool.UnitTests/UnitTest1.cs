226d2d3cfb70703a5403d798e3137e00f744b660
Aaron Schultz <schultzaaron@web.de>
2021-08-27 15:50:14 +0200

    Added unit test project

---
+++ StatisticsAnalysisTool/StatisticsAnalysisTool.UnitTests/StatisticsAnalysisTool.UnitTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace StatisticsAnalysisTool.UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
