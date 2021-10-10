aecc1eb7413772ccb1809f665b68689cd0da5464
Aaron Schultz <schultzaaron@web.de>
2021-10-11 00:01:34 +0200

    Fixes a bug in the WPF clipboard handler
    Fix #91

--- StatisticsAnalysisTool/StatisticsAnalysisTool/App.xaml.cs
+++ StatisticsAnalysisTool/StatisticsAnalysisTool/App.xaml.cs
@@ -1,11 +1,21 @@
-﻿using System.Windows;
+﻿using System.Runtime.InteropServices;
+using System.Windows.Threading;
 
 namespace StatisticsAnalysisTool
 {
     /// <summary>
     ///     Interaktionslogik für "App.xaml"
     /// </summary>
-    public partial class App : Application
+    public partial class App
     {
+        // Fixes a issue in the WPF clipboard handler.
+        // It is necessary to handle the unhandled exception in the Application.DispatcherUnhandledException event.
+        private void Application_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            if (e.Exception is COMException { ErrorCode: -2147221040 })
+            {
+                e.Handled = true;
+            }
+        }
     }
 }
