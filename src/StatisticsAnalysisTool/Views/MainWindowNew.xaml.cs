e877b50928b3969851b44e4c971b8302ef5c73d3
Aaron Schultz <schultzaaron@web.de>
2021-12-08 13:32:39 +0100

    Create a new main window

---
+++ src/StatisticsAnalysisTool/Views/MainWindowNew.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace StatisticsAnalysisTool.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindowNew.xaml
+    /// </summary>
+    public partial class MainWindowNew : Window
+    {
+        public MainWindowNew()
+        {
+            InitializeComponent();
+        }
+    }
+}
