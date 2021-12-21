8e379f30ebe3b9cb17260b9cacf9c83f03b4766b
Aaron Schultz <schultzaaron@web.de>
2021-12-21 23:34:08 +0100

    Added info window for dashboard

--- src/StatisticsAnalysisTool/UserControls/DashboardControl.xaml.cs
+++ src/StatisticsAnalysisTool/UserControls/DashboardControl.xaml.cs
@@ -1,4 +1,8 @@
-﻿namespace StatisticsAnalysisTool.UserControls
+﻿using System.Windows;
+using System.Windows.Input;
+using StatisticsAnalysisTool.ViewModels;
+
+namespace StatisticsAnalysisTool.UserControls
 {
     /// <summary>
     /// Interaction logic for DashboardControl.xaml
@@ -11,6 +15,24 @@
         }
 
         #region Ui events
+
+        private void BtnTrackingReset_Click(object sender, RoutedEventArgs e)
+        {
+            var vm = (MainWindowViewModel)DataContext;
+            vm.ResetMainCounters();
+        }
+
+        private void OpenMainTrackerInfoPopup_MouseEnter(object sender, MouseEventArgs e)
+        {
+            var vm = (MainWindowViewModel)DataContext;
+            vm.IsMainTrackerPopupVisible = Visibility.Visible;
+        }
+
+        private void CloseMainTrackerInfoPopup_MouseLeave(object sender, MouseEventArgs e)
+        {
+            var vm = (MainWindowViewModel)DataContext;
+            vm.IsMainTrackerPopupVisible = Visibility.Hidden;
+        }
 
         #endregion
     }
