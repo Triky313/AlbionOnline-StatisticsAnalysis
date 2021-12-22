c533c141e5e13f5cccd7c985adb78fa6d0f8d430
Aaron Schultz <schultzaaron@web.de>
2021-12-22 18:47:52 +0100

    Fix namespace

--- src/StatisticsAnalysisTool/Models/DashboardObject.cs
+++ src/StatisticsAnalysisTool/Models/DashboardObject.cs
@@ -4,238 +4,240 @@ using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
 
-namespace StatisticsAnalysisTool.Models;
-
-public class DashboardObject : INotifyPropertyChanged
+namespace StatisticsAnalysisTool.Models
 {
-    private double _famePerHour;
-    private double _reSpecPointsPerHour;
-    private double _silverPerHour;
-    private double _mightPerHour;
-    private double _favorPerHour;
-    private double _totalGainedFameInSessionInSession;
-    private double _totalGainedReSpecPointsInSessionInSession;
-    private double _totalGainedSilverInSessionInSession;
-    private double _totalGainedMightInSession;
-    private double _totalGainedFavorInSession;
-    private double _highestValue;
-    private double _fameInPercent;
-    private double _silverInPercent;
-    private double _reSpecPointsInPercent;
-    private double _mightInPercent;
-    private double _favorInPercent;
 
-    public double GetHighestValue()
+    public class DashboardObject : INotifyPropertyChanged
     {
-        var values = new List<double>()
+        private double _famePerHour;
+        private double _reSpecPointsPerHour;
+        private double _silverPerHour;
+        private double _mightPerHour;
+        private double _favorPerHour;
+        private double _totalGainedFameInSessionInSession;
+        private double _totalGainedReSpecPointsInSessionInSession;
+        private double _totalGainedSilverInSessionInSession;
+        private double _totalGainedMightInSession;
+        private double _totalGainedFavorInSession;
+        private double _highestValue;
+        private double _fameInPercent;
+        private double _silverInPercent;
+        private double _reSpecPointsInPercent;
+        private double _mightInPercent;
+        private double _favorInPercent;
+
+        public double GetHighestValue()
         {
-            TotalGainedFameInSession,
-            TotalGainedSilverInSession,
-            TotalGainedReSpecPointsInSession,
-            TotalGainedMightInSession,
-            TotalGainedFavorInSession
-        };
-        
-        return values.Max<double>();
-    }
+            var values = new List<double>()
+            {
+                TotalGainedFameInSession,
+                TotalGainedSilverInSession,
+                TotalGainedReSpecPointsInSession,
+                TotalGainedMightInSession,
+                TotalGainedFavorInSession
+            };
 
-    public void Reset()
-    {
-        HighestValue = 0;
-
-        FamePerHour = 0;
-        SilverPerHour = 0;
-        ReSpecPointsPerHour = 0;
-        MightPerHour = 0;
-        FavorPerHour = 0;
-
-        TotalGainedFameInSession = 0;
-        TotalGainedSilverInSession = 0;
-        TotalGainedReSpecPointsInSession = 0;
-        TotalGainedMightInSession = 0;
-        TotalGainedFavorInSession = 0;
-    }
-
-    public double HighestValue
-    {
-        get => _highestValue;
-        set
-        {
-            _highestValue = value;
-            OnPropertyChanged();
+            return values.Max<double>();
         }
-    }
-    
-    public double FamePerHour
-    {
-        get => _famePerHour;
-        set
+
+        public void Reset()
         {
-            _famePerHour = value;
-            OnPropertyChanged();
-        }
-    }
+            HighestValue = 0;
 
-    public double SilverPerHour
-    {
-        get => _silverPerHour;
-        set
+            FamePerHour = 0;
+            SilverPerHour = 0;
+            ReSpecPointsPerHour = 0;
+            MightPerHour = 0;
+            FavorPerHour = 0;
+
+            TotalGainedFameInSession = 0;
+            TotalGainedSilverInSession = 0;
+            TotalGainedReSpecPointsInSession = 0;
+            TotalGainedMightInSession = 0;
+            TotalGainedFavorInSession = 0;
+        }
+
+        public double HighestValue
         {
-            _silverPerHour = value;
-            OnPropertyChanged();
+            get => _highestValue;
+            set
+            {
+                _highestValue = value;
+                OnPropertyChanged();
+            }
         }
-    }
 
-    public double ReSpecPointsPerHour
-    {
-        get => _reSpecPointsPerHour;
-        set
+        public double FamePerHour
         {
-            _reSpecPointsPerHour = value;
-            OnPropertyChanged();
+            get => _famePerHour;
+            set
+            {
+                _famePerHour = value;
+                OnPropertyChanged();
+            }
         }
-    }
 
-    public double MightPerHour
-    {
-        get => _mightPerHour;
-        set
+        public double SilverPerHour
         {
-            _mightPerHour = value;
-            OnPropertyChanged();
+            get => _silverPerHour;
+            set
+            {
+                _silverPerHour = value;
+                OnPropertyChanged();
+            }
         }
-    }
 
-    public double FavorPerHour
-    {
-        get => _favorPerHour;
-        set
+        public double ReSpecPointsPerHour
         {
-            _favorPerHour = value;
-            OnPropertyChanged();
+            get => _reSpecPointsPerHour;
+            set
+            {
+                _reSpecPointsPerHour = value;
+                OnPropertyChanged();
+            }
         }
-    }
 
-    #region Percent values
-
-
-    public double FameInPercent
-    {
-        get => _fameInPercent;
-        set
+        public double MightPerHour
         {
-            _fameInPercent = value;
-            OnPropertyChanged();
+            get => _mightPerHour;
+            set
+            {
+                _mightPerHour = value;
+                OnPropertyChanged();
+            }
         }
-    }
 
-    public double SilverInPercent
-    {
-        get => _silverInPercent;
-        set
+        public double FavorPerHour
         {
-            _silverInPercent = value;
-            OnPropertyChanged();
+            get => _favorPerHour;
+            set
+            {
+                _favorPerHour = value;
+                OnPropertyChanged();
+            }
         }
-    }
 
-    public double ReSpecPointsInPercent
-    {
-        get => _reSpecPointsInPercent;
-        set
+        #region Percent values
+
+
+        public double FameInPercent
         {
-            _reSpecPointsInPercent = value;
-            OnPropertyChanged();
+            get => _fameInPercent;
+            set
+            {
+                _fameInPercent = value;
+                OnPropertyChanged();
+            }
         }
-    }
 
-    public double MightInPercent
-    {
-        get => _mightInPercent;
-        set
+        public double SilverInPercent
         {
-            _mightInPercent = value;
-            OnPropertyChanged();
+            get => _silverInPercent;
+            set
+            {
+                _silverInPercent = value;
+                OnPropertyChanged();
+            }
         }
-    }
 
-    public double FavorInPercent
-    {
-        get => _favorInPercent;
-        set
+        public double ReSpecPointsInPercent
         {
-            _favorInPercent = value;
-            OnPropertyChanged();
+            get => _reSpecPointsInPercent;
+            set
+            {
+                _reSpecPointsInPercent = value;
+                OnPropertyChanged();
+            }
         }
-    }
 
-    #endregion
-
-    public double TotalGainedFameInSession
-    {
-        get => _totalGainedFameInSessionInSession;
-        set
+        public double MightInPercent
         {
-            _totalGainedFameInSessionInSession = value;
-            HighestValue = GetHighestValue();
-            FameInPercent = _totalGainedFameInSessionInSession / HighestValue * 100;
-            OnPropertyChanged();
+            get => _mightInPercent;
+            set
+            {
+                _mightInPercent = value;
+                OnPropertyChanged();
+            }
         }
-    }
 
-    public double TotalGainedSilverInSession
-    {
-        get => _totalGainedSilverInSessionInSession;
-        set
+        public double FavorInPercent
         {
-            _totalGainedSilverInSessionInSession = value;
-            HighestValue = GetHighestValue();
-            SilverInPercent = _totalGainedSilverInSessionInSession / HighestValue * 100;
-            OnPropertyChanged();
+            get => _favorInPercent;
+            set
+            {
+                _favorInPercent = value;
+                OnPropertyChanged();
+            }
         }
-    }
 
-    public double TotalGainedReSpecPointsInSession
-    {
-        get => _totalGainedReSpecPointsInSessionInSession;
-        set
+        #endregion
+
+        public double TotalGainedFameInSession
         {
-            _totalGainedReSpecPointsInSessionInSession = value;
-            HighestValue = GetHighestValue();
-            ReSpecPointsInPercent = _totalGainedReSpecPointsInSessionInSession / HighestValue * 100;
-            OnPropertyChanged();
+            get => _totalGainedFameInSessionInSession;
+            set
+            {
+                _totalGainedFameInSessionInSession = value;
+                HighestValue = GetHighestValue();
+                FameInPercent = _totalGainedFameInSessionInSession / HighestValue * 100;
+                OnPropertyChanged();
+            }
         }
-    }
 
-    public double TotalGainedMightInSession
-    {
-        get => _totalGainedMightInSession;
-        set
+        public double TotalGainedSilverInSession
         {
-            _totalGainedMightInSession = value;
-            HighestValue = GetHighestValue();
-            MightInPercent = _totalGainedMightInSession / HighestValue * 100;
-            OnPropertyChanged();
+            get => _totalGainedSilverInSessionInSession;
+            set
+            {
+                _totalGainedSilverInSessionInSession = value;
+                HighestValue = GetHighestValue();
+                SilverInPercent = _totalGainedSilverInSessionInSession / HighestValue * 100;
+                OnPropertyChanged();
+            }
         }
-    }
 
-    public double TotalGainedFavorInSession
-    {
-        get => _totalGainedFavorInSession;
-        set
+        public double TotalGainedReSpecPointsInSession
         {
-            _totalGainedFavorInSession = value;
-            HighestValue = GetHighestValue();
-            FavorInPercent = _totalGainedFavorInSession / HighestValue * 100;
-            OnPropertyChanged();
+            get => _totalGainedReSpecPointsInSessionInSession;
+            set
+            {
+                _totalGainedReSpecPointsInSessionInSession = value;
+                HighestValue = GetHighestValue();
+                ReSpecPointsInPercent = _totalGainedReSpecPointsInSessionInSession / HighestValue * 100;
+                OnPropertyChanged();
+            }
         }
-    }
 
-    public event PropertyChangedEventHandler PropertyChanged;
+        public double TotalGainedMightInSession
+        {
+            get => _totalGainedMightInSession;
+            set
+            {
+                _totalGainedMightInSession = value;
+                HighestValue = GetHighestValue();
+                MightInPercent = _totalGainedMightInSession / HighestValue * 100;
+                OnPropertyChanged();
+            }
+        }
 
-    [NotifyPropertyChangedInvocator]
-    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-    {
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        public double TotalGainedFavorInSession
+        {
+            get => _totalGainedFavorInSession;
+            set
+            {
+                _totalGainedFavorInSession = value;
+                HighestValue = GetHighestValue();
+                FavorInPercent = _totalGainedFavorInSession / HighestValue * 100;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
