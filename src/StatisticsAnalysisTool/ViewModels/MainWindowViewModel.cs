47a51c855dccb5e085ce1f373dfff8ef6d97a58c
Aaron Schultz <schultzaaron@web.de>
2022-01-16 21:17:49 +0100

    Fix a problem with UpdateItemsJsonByDaysSelection

--- src/StatisticsAnalysisTool/ViewModels/MainWindowViewModel.cs
+++ src/StatisticsAnalysisTool/ViewModels/MainWindowViewModel.cs
@@ -19,7 +19,6 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
