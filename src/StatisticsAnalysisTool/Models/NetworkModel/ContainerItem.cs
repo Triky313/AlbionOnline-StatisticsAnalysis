85b7659f1026fdc7c4c1188bdefe8c39ed1c1c21
Aaron <schultzaaron@web.de>
2022-04-20 17:20:21 +0200

    Storage json file downsized

--- src/StatisticsAnalysisTool/Models/NetworkModel/ContainerItem.cs
+++ src/StatisticsAnalysisTool/Models/NetworkModel/ContainerItem.cs
@@ -5,8 +5,10 @@ namespace StatisticsAnalysisTool.Models.NetworkModel;
 
 public class ContainerItem
 {
+    [JsonPropertyName("I")]
     public int ItemIndex { get; set; }
     [JsonIgnore]
     public Item Item => ItemController.GetItemByIndex(ItemIndex);
+    [JsonPropertyName("Q")]
     public int Quantity { get; set; }
 }
