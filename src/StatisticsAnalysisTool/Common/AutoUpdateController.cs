e4469b50870b51b445eaee7101887f204da10fbf
Aaron <schultzaaron@web.de>
2022-05-04 13:12:02 +0200

    Automatically remove update files that are no longer required
    Close #141

--- src/StatisticsAnalysisTool/Common/AutoUpdateController.cs
+++ src/StatisticsAnalysisTool/Common/AutoUpdateController.cs
@@ -1,7 +1,10 @@
 ﻿using AutoUpdaterDotNET;
+using log4net;
 using StatisticsAnalysisTool.Common.UserSettings;
 using StatisticsAnalysisTool.Properties;
 using System;
+using System.IO;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -9,8 +12,12 @@ namespace StatisticsAnalysisTool.Common;
 
 public static class AutoUpdateController
 {
+    private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod()?.DeclaringType);
+
     public static void AutoUpdate()
     {
+        RemoveUpdateFiles();
+
         if (SettingsController.CurrentSettings.IsSuggestPreReleaseUpdatesActive)
         {
 #pragma warning disable CA1416 // Validate platform compatibility
@@ -35,5 +42,31 @@ public static class AutoUpdateController
     {
         await Task.Delay(3000);
         Application.Current.Shutdown();
+    }
+
+    public static void RemoveUpdateFiles()
+    {
+        var localFilePath = AppDomain.CurrentDomain.BaseDirectory;
+
+        try
+        {
+            foreach (var filePath in Directory.GetFiles(localFilePath, "StatisticsAnalysis-AlbionOnline-*-x64.zip"))
+            {
+                if (File.Exists(filePath))
+                {
+                    File.Delete(filePath);
+                }
+            }
+        }
+        catch (Exception ex) when (ex is DirectoryNotFoundException or UnauthorizedAccessException or PathTooLongException)
+        {
+            ConsoleManager.WriteLineForError(MethodBase.GetCurrentMethod()?.DeclaringType, ex);
+            Log.Warn($"{MethodBase.GetCurrentMethod()?.DeclaringType}: {ex.Message}");
+        }
+        catch (Exception e)
+        {
+            ConsoleManager.WriteLineForError(MethodBase.GetCurrentMethod()?.DeclaringType, e);
+            Log.Error($"{MethodBase.GetCurrentMethod()?.DeclaringType}: {e.Message}");
+        }
     }
 }
