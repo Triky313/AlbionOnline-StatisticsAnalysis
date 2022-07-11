f62a2065d7578eee27f55973567a2af6298e3328
Aaron <schultzaaron@web.de>
2022-07-12 00:58:00 +0200

    Fix dashboard text translations

--- src/StatisticsAnalysisTool/ViewModels/DashboardWindowViewModel.cs
+++ src/StatisticsAnalysisTool/ViewModels/DashboardWindowViewModel.cs
@@ -2,13 +2,11 @@
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
-using StatisticsAnalysisTool.Models.TranslationModel;
 
 namespace StatisticsAnalysisTool.ViewModels
 {
     public class DashboardWindowViewModel : INotifyPropertyChanged
     {
-        private DashboardWindowTranslation _translation;
         private DashboardBindings _dashboardBindings;
         private ObservableCollection<MainStatObject> _factionPointStats;
 
@@ -16,14 +14,8 @@ namespace StatisticsAnalysisTool.ViewModels
         {
             DashboardBindings = dashboardBindings;
             FactionPointStats = factionPointStats;
-            Init();
         }
-
-        private void Init()
-        {
-            Translation = new DashboardWindowTranslation();
-        }
-
+        
         public DashboardBindings DashboardBindings
         {
             get => _dashboardBindings;
@@ -43,17 +35,7 @@ namespace StatisticsAnalysisTool.ViewModels
                 OnPropertyChanged();
             }
         }
-
-        public DashboardWindowTranslation Translation
-        {
-            get => _translation;
-            set
-            {
-                _translation = value;
-                OnPropertyChanged();
-            }
-        }
-
+        
         public event PropertyChangedEventHandler PropertyChanged;
 
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
