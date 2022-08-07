82ac4ff89d31f4716b6fac7cc060e5f21d1dafb8
Aaron <schultzaaron@web.de>
2022-08-07 17:50:13 +0200

    Version increased

--- src/StatisticsAnalysisTool/Properties/AssemblyInfo.cs
+++ src/StatisticsAnalysisTool/Properties/AssemblyInfo.cs
@@ -53,5 +53,5 @@ using System.Windows;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // indem Sie "*" wie unten gezeigt eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("5.11.0.*")]
-[assembly: AssemblyFileVersion("5.11.0.0")]
+[assembly: AssemblyVersion("5.11.1.*")]
+[assembly: AssemblyFileVersion("5.11.1.0")]
