e6206875c2b7deaf7777e8d104a1a6514f5a0d25
Aaron <schultzaaron@web.de>
2022-08-11 23:31:49 +0200

    Added loot logger csv file format description

--- src/StatisticsAnalysisTool/Models/LootLoggerObject.cs
+++ src/StatisticsAnalysisTool/Models/LootLoggerObject.cs
@@ -21,6 +21,18 @@ namespace StatisticsAnalysisTool.Models
         public string LootedByName { get; set; }
         public string LootedByGuild { get; set; }
         public string LootedByAlliance { get; set; }
+
+        // CSV Format for https://matheus.sampaio.us/ao-loot-logger-viewer/
+        // 'timestamp_utc'          (ISO8601 format, example: `2019-09-07T-15:50+00`)
+        // 'looted_by__alliance'    (can be empty),
+        // 'looted_by__guild'       (can be empty),
+        // 'looted_by__name',
+        // 'item_id'                (example `T8_SHOES_LEATHER_ROYAL`),
+        // 'item_name'              (example `Ancient Royal Shoes`),
+        // 'quantity',
+        // 'looted_from__alliance'  (can be empty),
+        // 'looted_from__guild'     (can be empty),
+        // 'looted_from__name'
         public string CsvOutput => $"{UtcPickupTime.ToString("O", CultureInfo.InvariantCulture)};{LootedByAlliance};{LootedByGuild};{LootedByName};{ItemId};{UniqueItemName};{Quantity}" +
                                    $";{LootedFromAlliance};{LootedFromGuild};{LootedFromName}";
         public string CsvOutputWithRealItemName => GetCsvOutputStringWithRealItemName();
