9c3ac7dcb8b4b3623382ee843023abb4a328efd2
Aaron <schultzaaron@web.de>
2022-09-30 16:26:42 +0200

    Added KilledByGuild to kill notifications

--- src/StatisticsAnalysisTool/Network/Notification/KillNotificationFragment.cs
+++ src/StatisticsAnalysisTool/Network/Notification/KillNotificationFragment.cs
@@ -1,16 +1,21 @@
-﻿namespace StatisticsAnalysisTool.Network.Notification
-{
-    public class KillNotificationFragment : LineFragment
-    {
-        public KillNotificationFragment(string died, string killedBy, string valueText)
-        {
-            Died = died;
-            KilledBy = killedBy;
-            ValueText = valueText;
-        }
+﻿using System.Text.Json.Serialization;
 
-        public string Died { get; }
-        public string KilledBy { get; }
-        public string ValueText { get; }
+namespace StatisticsAnalysisTool.Network.Notification;
+
+public class KillNotificationFragment : LineFragment
+{
+    public KillNotificationFragment(string died, string killedBy, string killedByGuild, string valueText)
+    {
+        Died = died;
+        KilledBy = killedBy;
+        KilledByGuild = killedByGuild;
+        ValueText = valueText;
     }
+
+    public string Died { get; }
+    public string KilledBy { get; }
+    public string KilledByGuild { get; }
+    [JsonIgnore] 
+    public bool IsKilledByGuildEmpty => string.IsNullOrEmpty(KilledByGuild);
+    public string ValueText { get; }
 }
