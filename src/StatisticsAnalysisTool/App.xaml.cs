d426c3a87d5dafff4bf3fb6eab6d36e677702433
Aaron <schultzaaron@web.de>
2023-01-21 15:51:21 +0100

    Remove register service

--- src/StatisticsAnalysisTool/App.xaml.cs
+++ src/StatisticsAnalysisTool/App.xaml.cs
@@ -1,18 +1,10 @@
-﻿using StatisticsAnalysisTool.Common;
-using System.Runtime.InteropServices;
-using System.Windows;
+﻿using System.Runtime.InteropServices;
 using System.Windows.Threading;
 
 namespace StatisticsAnalysisTool;
 
 public partial class App
 {
-    protected override void OnStartup(StartupEventArgs e)
-    {
-        base.OnStartup(e);
-        _ = ServiceRegistration.RegisterServices();
-    }
-
     // Fixes a issue in the WPF clipboard handler.
     // It is necessary to handle the unhandled exception in the Application.DispatcherUnhandledException event.
     private void Application_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
