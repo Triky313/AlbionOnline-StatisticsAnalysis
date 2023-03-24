dc3dc41ea469d1aa2ce19824f8e3affc9dc9bed1
PurpleGale <90148755+PurpleGale@users.noreply.github.com>
2023-03-25 00:09:10 +0300

    Update DownloadItemJsonFilesManually.bat

--- src/StatisticsAnalysisTool/DownloadItemJsonFilesManually.bat
+++ src/StatisticsAnalysisTool/DownloadItemJsonFilesManually.bat
@@ -1,3 +1,3 @@
-powershell -Command "if(!(Test-Path -Path 'Temp')) { New-Item -ItemType Directory -Path 'Temp' } ; Invoke-WebRequest https://raw.githubusercontent.com/ao-data/ao-bin-dumps/master/formatted/items.json -OutFile ItemList.json"
-powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/ao-data/ao-bin-dumps/master/items.json  -OutFile Items.json"
-powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/ao-data/ao-bin-dumps/master/mobs.json  -OutFile Temp\mobs.json"
+powershell -Command "if(!(Test-Path -Path 'Temp')) { New-Item -ItemType Directory -Path 'Temp' } ; Invoke-WebRequest https://raw.githubusercontent.com/Triky313/ao-bin-dumps/main/formatted/items.json -OutFile ItemList.json"
+powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/Triky313/ao-bin-dumps/main/items.json  -OutFile Items.json"
+powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/Triky313/ao-bin-dumps/main/mobs.json  -OutFile Temp\mobs.json"
