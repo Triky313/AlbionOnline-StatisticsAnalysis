99e8b1394ba3acf77e5da9e0e8d7744ff7aa18ed
Aaron <schultzaaron@web.de>
2023-07-03 18:20:49 +0200

    Fixed a party join bug with new PartyJoinedEvent

---
+++ src/StatisticsAnalysisTool/Network/Events/PartyJoinedEvent.cs
@@ -0,0 +1,40 @@
+﻿using StatisticsAnalysisTool.Common;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace StatisticsAnalysisTool.Network.Events;
+
+public class PartyJoinedEvent
+{
+    public Guid UserGuid { get; private set; } = Guid.Empty;
+    public string Username { get; private set; } = string.Empty;
+    public string GuildName { get; private set; } = string.Empty;
+
+    public PartyJoinedEvent(Dictionary<byte, object> parameters)
+    {
+        ConsoleManager.WriteLineForNetworkHandler(GetType().Name, parameters);
+
+        try
+        {
+            if (parameters.TryGetValue(1, out object userGuid) && userGuid is Guid)
+            {
+                UserGuid = userGuid.ObjectToGuid() ?? Guid.Empty;
+            }
+
+            if (parameters.TryGetValue(1, out object username) && username is string usernameString && !string.IsNullOrEmpty(usernameString))
+            {
+                Username = usernameString;
+            }
+
+            if (parameters.TryGetValue(5, out object guildName) && guildName is string guildNameString && !string.IsNullOrEmpty(guildNameString))
+            {
+                GuildName = guildNameString;
+            }
+        }
+        catch (Exception e)
+        {
+            ConsoleManager.WriteLineForError(MethodBase.GetCurrentMethod()?.DeclaringType, e);
+        }
+    }
+}
