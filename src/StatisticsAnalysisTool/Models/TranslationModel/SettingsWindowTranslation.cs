56ce9b7445f7e044f5134dafa34be254447b305b
Aaron <schultzaaron@web.de>
2023-07-21 15:11:57 +0200

    Added update spells settings

--- src/StatisticsAnalysisTool/Models/TranslationModel/SettingsWindowTranslation.cs
+++ src/StatisticsAnalysisTool/Models/TranslationModel/SettingsWindowTranslation.cs
@@ -14,10 +14,12 @@ public class SettingsWindowTranslation
     public static string UpdateItemsJsonByDays => LanguageController.Translation("UPDATE_ITEMS_JSON_BY_DAYS");
     public static string UpdateMobsJsonByDays => LanguageController.Translation("UPDATE_MOBS_JSON_BY_DAYS");
     public static string UpdateWorldJsonByDays => LanguageController.Translation("UPDATE_WORLD_JSON_BY_DAYS");
+    public static string UpdateSpellsJsonByDays => LanguageController.Translation("UPDATE_SPELLS_JSON_BY_DAYS");
     public static string ItemListSourceUrl => LanguageController.Translation("ITEM_LIST_SOURCE_URL");
     public static string ItemsJsonSourceUrl => LanguageController.Translation("ITEMS_JSON_SOURCE_URL");
     public static string MobsJsonSourceUrl => LanguageController.Translation("MOBS_JSON_SOURCE_URL");
     public static string WorldJsonSourceUrl => LanguageController.Translation("WORLD_JSON_SOURCE_URL");
+    public static string SpellsJsonSourceUrl => LanguageController.Translation("SPELLS_JSON_SOURCE_URL");
     public static string OpenItemWindowInNewWindow => LanguageController.Translation("OPEN_ITEM_WINDOW_IN_NEW_WINDOW");
     public static string ShowInfoWindowOnStart => LanguageController.Translation("SHOW_INFO_WINDOW_ON_START");
     public static string Save => LanguageController.Translation("SAVE");
