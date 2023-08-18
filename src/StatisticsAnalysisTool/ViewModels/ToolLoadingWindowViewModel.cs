978331c6bc435d59d93b461f576276fa5bef761d
Aaron <schultzaaron@web.de>
2023-08-18 10:29:04 +0200

    Added loading window with progressbar for the tool

---
+++ src/StatisticsAnalysisTool/ViewModels/ToolLoadingWindowViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using StatisticsAnalysisTool.Common;
+
+namespace StatisticsAnalysisTool.ViewModels;
+
+public class ToolLoadingWindowViewModel : INotifyPropertyChanged
+{
+    private double _progressBarValue;
+    
+    public double ProgressBarValue
+    {
+        get => _progressBarValue;
+        set
+        {
+            _progressBarValue = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public static string TranslationLoading => LanguageController.Translation("LOADING");
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
