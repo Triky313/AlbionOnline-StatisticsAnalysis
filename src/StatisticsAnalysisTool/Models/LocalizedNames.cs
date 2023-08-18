9558f67e69b12a4bc03e5825c63b9d3cdc02f7e2
Aaron <schultzaaron@web.de>
2023-08-18 11:17:49 +0200

    Added Japan, Taiwan, Indonesian as item name language

--- src/StatisticsAnalysisTool/Models/LocalizedNames.cs
+++ src/StatisticsAnalysisTool/Models/LocalizedNames.cs
@@ -6,24 +6,16 @@ public class LocalizedNames
 {
     // ISO 639-1 standard language codes
     [JsonPropertyName("EN-US")] public string EnUs { get; set; }
-
     [JsonPropertyName("DE-DE")] public string DeDe { get; set; }
-
     [JsonPropertyName("KO-KR")] public string KoKr { get; set; }
-
     [JsonPropertyName("RU-RU")] public string RuRu { get; set; }
-
     [JsonPropertyName("PL-PL")] public string PlPl { get; set; }
-
     [JsonPropertyName("PT-BR")] public string PtBr { get; set; }
-
     [JsonPropertyName("FR-FR")] public string FrFr { get; set; }
-
     [JsonPropertyName("ES-ES")] public string EsEs { get; set; }
-
     [JsonPropertyName("ZH-CN")] public string ZhCn { get; set; }
-
     [JsonPropertyName("IT-IT")] public string ItIt { get; set; }
-
     [JsonPropertyName("JA-JP")] public string JaJp { get; set; }
+    [JsonPropertyName("ZH-TW")] public string ZhTw { get; set; }
+    [JsonPropertyName("ID-ID")] public string IdId { get; set; }
 }
