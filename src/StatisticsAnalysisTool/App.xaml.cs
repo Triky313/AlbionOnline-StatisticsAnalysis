17910da1b19243549e44b658106e70c13fd77bd2
Aaron <schultzaaron@web.de>
2023-09-22 22:11:19 +0200

    Set Auto updater to Synchronous so the update window starts first

--- src/StatisticsAnalysisTool/App.xaml.cs
+++ src/StatisticsAnalysisTool/App.xaml.cs
@@ -33,6 +33,8 @@ public partial class App
 
         AppDomain.CurrentDomain.UnhandledException += OnUnhandledException;
 
+        await AutoUpdateController.AutoUpdateAsync();
+
         SettingsController.LoadSettings();
         InitializeLanguage();
 
@@ -49,7 +51,6 @@ public partial class App
         await _mainWindowViewModel.InitMainWindowDataAsync();
         Current.MainWindow.Show();
 
-        await AutoUpdateController.AutoUpdateAsync();
 
         Utilities.AnotherAppToStart(SettingsController.CurrentSettings.AnotherAppToStartPath);
     }
