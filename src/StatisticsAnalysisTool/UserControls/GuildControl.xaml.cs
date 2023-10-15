24e386305097af2e637bd03a1d151127c0a71523
Aaron <schultzaaron@web.de>
2023-10-16 00:26:21 +0200

    Added guild tab + guild styles + siphoned energy tab in guild tab

---
+++ src/StatisticsAnalysisTool/UserControls/GuildControl.xaml.cs
@@ -0,0 +1,40 @@
+﻿using StatisticsAnalysisTool.ViewModels;
+using System.Windows;
+using System.Windows.Input;
+
+namespace StatisticsAnalysisTool.UserControls;
+
+/// <summary>
+/// Interaction logic for LoggingControl.xaml
+/// </summary>
+public partial class GuildControl
+{
+    private readonly GuildViewModel _guildViewModel;
+
+    public GuildControl()
+    {
+        InitializeComponent();
+        _guildViewModel = new GuildViewModel();
+        DataContext = _guildViewModel;
+    }
+
+    private void BtnDeleteSelectedSiphonedEnergyEntries_Click(object sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void OpenSiphonedEnergyInfoPopup_MouseEnter(object sender, MouseEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void CloseSiphonedEnergyInfoPopup_MouseLeave(object sender, MouseEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void BtnSelectSwitchAllSiphonedEnergyEntries_Click(object sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ src/StatisticsAnalysisTool/ViewModels/GuildViewModel.cs
@@ -0,0 +1,12 @@
+﻿using StatisticsAnalysisTool.Localization;
+
+namespace StatisticsAnalysisTool.ViewModels;
+
+public class GuildViewModel : BaseViewModel
+{
+
+    public string TranslationSiphonedEnergy => LanguageController.Translation("SIPHONED_ENERGY");
+    public string TranslationDeleteSelectedEntries => LanguageController.Translation("DELETE_SELECTED_ENTRIES");
+    public string TranslationSelectDeselectAll => LanguageController.Translation("SELECT_DESELECT_ALL");
+    public string TranslationLastUpdate => LanguageController.Translation("LAST_UPDATE");
+}
