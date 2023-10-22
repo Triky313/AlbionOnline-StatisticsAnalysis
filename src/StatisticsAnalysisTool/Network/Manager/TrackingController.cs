6e70b23a4b0da23ab2a8507f4fb407943021f630
Aaron <schultzaaron@web.de>
2023-10-22 14:26:06 +0200

    Admin constraint disabled

--- src/StatisticsAnalysisTool/Network/Manager/TrackingController.cs
+++ src/StatisticsAnalysisTool/Network/Manager/TrackingController.cs
@@ -2,7 +2,6 @@ using Serilog;
 using StatisticsAnalysisTool.Cluster;
 using StatisticsAnalysisTool.Common;
 using StatisticsAnalysisTool.Common.UserSettings;
-using StatisticsAnalysisTool.Core;
 using StatisticsAnalysisTool.Dungeon;
 using StatisticsAnalysisTool.EstimatedMarketValue;
 using StatisticsAnalysisTool.EventLogging;
@@ -105,11 +104,6 @@ public class TrackingController : ITrackingController
         }
 
         _networkManager = new NetworkManager(this);
-
-        if (!ApplicationCore.IsAppStartedAsAdministrator())
-        {
-            _mainWindowViewModel.SetErrorBar(Visibility.Visible, LanguageController.Translation("START_APPLICATION_AS_ADMINISTRATOR"));
-        }
 
         await Task.WhenAll(
             EstimatedMarketValueController.LoadFromFileAsync(),
