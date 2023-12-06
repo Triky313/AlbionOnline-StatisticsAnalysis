2cf7674b194626c7f486369d969336829dea5fac
Aaron <schultzaaron@web.de>
2023-12-06 18:35:38 +0100

    Update interface

--- src/StatisticsAnalysisTool.Avalonia/ViewModels/Interfaces/IMainWindowHeaderViewModel.cs
+++ src/StatisticsAnalysisTool.Avalonia/ViewModels/Interfaces/IMainWindowHeaderViewModel.cs
@@ -1,5 +1,8 @@
-﻿namespace StatisticsAnalysisTool.Avalonia.ViewModels.Interfaces;
+﻿using StatisticsAnalysisTool.Avalonia.Network.Enums;
+
+namespace StatisticsAnalysisTool.Avalonia.ViewModels.Interfaces;
 
 public interface IMainWindowHeaderViewModel
 {
+    void UpdateTrackingStatusType(TrackingStatusType trackingStatusType);
 }
