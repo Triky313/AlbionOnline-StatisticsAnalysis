fc7a9ebbc0f25e7edf3f7dc7a9a4eb00f0082aca
Aaron <schultzaaron@web.de>
2024-06-29 15:11:27 +0200

    Added new tests

--- src/StatisticsAnalysisTool/Properties/AssemblyInfo.cs
+++ src/StatisticsAnalysisTool/Properties/AssemblyInfo.cs
@@ -2,6 +2,7 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Runtime.Versioning;
 using System.Windows;
+using System.Runtime.CompilerServices;
 
 // Allgemeine Informationen über eine Assembly werden über die folgenden
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
@@ -38,6 +39,9 @@ using System.Windows;
                                               //(wird verwendet, wenn eine Ressource auf der Seite nicht gefunden wird,
                                               // designspezifischen Ressourcenwörterbuch nicht gefunden werden kann.)
 )]
+
+// Tests
+[assembly: InternalsVisibleTo("StatisticsAnalysisTool.Tests")]
 
 // Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
 //
