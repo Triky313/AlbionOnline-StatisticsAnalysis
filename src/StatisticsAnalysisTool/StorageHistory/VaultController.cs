deda0d72b0900bf01b51f9ce00126206998e2fb6
Aaron <schultzaaron@web.de>
2024-08-15 11:07:18 +0200

    Fixed a null ref bug

--- src/StatisticsAnalysisTool/StorageHistory/VaultController.cs
+++ src/StatisticsAnalysisTool/StorageHistory/VaultController.cs
@@ -363,7 +363,7 @@ public class VaultController
 
         var vaultDtosToSave = _vaultBindings.Vaults
             ?.ToList()
-            .Select(StorageHistoryMapping.Mapping);
+            .Select(StorageHistoryMapping.Mapping) ?? Enumerable.Empty<VaultDto>(); ;
 
         await FileController.SaveAsync(vaultDtosToSave,
             Path.Combine(AppDomain.CurrentDomain.BaseDirectory, Settings.Default.UserDataDirectoryName, Settings.Default.VaultsFileName));
