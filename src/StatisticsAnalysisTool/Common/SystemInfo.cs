5541c827c28fa8eec3c9723f498faeb1737a8e54
Aaron <schultzaaron@web.de>
2024-12-01 11:07:59 +0100

    Fixed a tool crash in systeminfo class

--- src/StatisticsAnalysisTool/Common/SystemInfo.cs
+++ src/StatisticsAnalysisTool/Common/SystemInfo.cs
@@ -1,4 +1,5 @@
 ﻿using Serilog;
+using System;
 using System.Diagnostics;
 using System.Linq;
 using System.Management;
@@ -12,20 +13,50 @@ public class SystemInfo
 {
     public static void LogSystemInfo()
     {
-        var osVersion = RuntimeInformation.OSDescription;
+        try
+        {
+            var osVersion = RuntimeInformation.OSDescription;
+            var osArchitecture = RuntimeInformation.OSArchitecture;
+            var isAdmin = new WindowsPrincipal(WindowsIdentity.GetCurrent()).IsInRole(WindowsBuiltInRole.Administrator);
+            var isDebug = Debugger.IsAttached;
 
-        var processor = new ManagementObjectSearcher("select * from Win32_Processor").Get().OfType<ManagementObject>().FirstOrDefault()?["Name"].ToString();
+            string processor = "Unknown";
+            try
+            {
+                processor = new ManagementObjectSearcher("SELECT Name FROM Win32_Processor")
+                    .Get()
+                    .OfType<ManagementObject>()
+                    .FirstOrDefault()?["Name"]?.ToString() ?? "Unknown";
+            }
+            catch (Exception ex)
+            {
+                Log.Warning("Failed to retrieve processor info: {Error}", ex.Message);
+            }
 
-        var videoController = new ManagementObjectSearcher("select * from Win32_VideoController").Get().OfType<ManagementObject>().FirstOrDefault()?["Name"].ToString();
+            string videoController = "Unknown";
+            try
+            {
+                videoController = new ManagementObjectSearcher("SELECT Name FROM Win32_VideoController")
+                    .Get()
+                    .OfType<ManagementObject>()
+                    .FirstOrDefault()?["Name"]?.ToString() ?? "Unknown";
+            }
+            catch (Exception ex)
+            {
+                Log.Warning("Failed to retrieve video controller info: {Error}", ex.Message);
+            }
 
-        var isAdmin = new WindowsPrincipal(WindowsIdentity.GetCurrent()).IsInRole(WindowsBuiltInRole.Administrator);
+            var activeNetworks = NetworkInterface.GetAllNetworkInterfaces()
+                .Where(nic => nic.OperationalStatus == OperationalStatus.Up)
+                .Select(nic => nic.Name)
+                .ToList();
 
-        var isDebug = Debugger.IsAttached;
-
-        var osArchitecture = RuntimeInformation.OSArchitecture;
-
-        var activeNetworks = NetworkInterface.GetAllNetworkInterfaces().Where(nic => nic.OperationalStatus == OperationalStatus.Up).Select(nic => nic.Name).ToList();
-
-        Log.Information($"OS Version: {osVersion} - {osArchitecture} | Processor: {processor} | Video: {videoController} | Is Admin: {isAdmin} | Is Debug: {isDebug} | Active Networks: {string.Join(", ", activeNetworks)}");
+            Log.Information("OS Version: {OSVersion} - {OSArchitecture} | Processor: {Processor} | Video: {VideoController} | Is Admin: {IsAdmin} | Is Debug: {IsDebug} | Active Networks: {ActiveNetworks}",
+                osVersion, osArchitecture, processor, videoController, isAdmin, isDebug, string.Join(", ", activeNetworks));
+        }
+        catch (Exception ex)
+        {
+            Log.Error("An unexpected error occurred in LogSystemInfo: {Error}", ex.Message);
+        }
     }
 }
