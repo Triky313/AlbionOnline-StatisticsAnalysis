580466a1b73eaccfc1cb007edc0092657d932045
Aaron <schultzaaron@web.de>
2025-03-01 18:22:57 +0100

    Fixed a validation binding error

---
+++ src/StatisticsAnalysisTool/Common/Converters/ValidationErrorConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace StatisticsAnalysisTool.Common.Converters;
+
+public class ValidationErrorConverter : IMultiValueConverter
+{
+    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (values[0] is System.Collections.ObjectModel.ReadOnlyObservableCollection<ValidationError> errors && errors.Count > 0)
+        {
+            return errors[0].ErrorContent;
+        }
+
+        return null;
+    }
+
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
