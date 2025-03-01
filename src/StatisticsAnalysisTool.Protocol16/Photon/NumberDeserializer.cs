c4cd4b3190ef719c8bdfda78cfc4dea72405e18d
Aaron <schultzaaron@web.de>
2025-03-01 10:39:59 +0100

    Performance improvement

--- src/StatisticsAnalysisTool.Protocol16/Photon/NumberDeserializer.cs
+++ src/StatisticsAnalysisTool.Protocol16/Photon/NumberDeserializer.cs
@@ -5,19 +5,15 @@ public class NumberDeserializer
     public static bool Deserialize(out int value, byte[] source, ref int offset)
     {
         value = 0;
+
         if (offset + 4 > source.Length)
         {
             return false;
         }
 
-        int value1 = source[offset] << 24;
-        offset++;
-        int value2 = value1 | source[offset] << 16;
-        offset++;
-        int value3 = value2 | source[offset] << 8;
-        offset++;
-        value = (value3 | source[offset]);
-        offset++;
+        var span = new Span<byte>(source, offset, 4);
+        value = (span[0] << 24) | (span[1] << 16) | (span[2] << 8) | span[3];
+        offset += 4;
 
         return true;
     }
@@ -25,15 +21,15 @@ public class NumberDeserializer
     public static bool Deserialize(out short value, byte[] source, ref int offset)
     {
         value = 0;
+
         if (offset + 2 > source.Length)
         {
             return false;
         }
 
-        short value1 = (short) (source[offset] << 8);
-        offset++;
-        value = (short) (value1 | (short) source[offset]);
-        offset++;
+        var span = new Span<byte>(source, offset, 2);
+        value = (short) ((span[0] << 8) | span[1]);
+        offset += 2;
 
         return true;
     }
