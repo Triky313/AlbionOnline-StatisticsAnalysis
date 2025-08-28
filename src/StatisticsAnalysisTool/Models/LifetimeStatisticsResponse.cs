acc1ccdc33d826364c8d1883fbe37dde126a7bb7
Aaron <schultzaaron@web.de>
2025-08-28 11:52:03 +0200

    API Controller: Makes Timestamp nullable
    Allows the Timestamp property in the LifetimeStatisticsResponse model to be null.
    This addresses potential scenarios where a timestamp might not be available.

--- src/StatisticsAnalysisTool/Models/LifetimeStatisticsResponse.cs
+++ src/StatisticsAnalysisTool/Models/LifetimeStatisticsResponse.cs
@@ -8,5 +8,5 @@ public class LifetimeStatisticsResponse
     public GatheringResponse Gathering { get; set; }
     public CraftingResponse Crafting { get; set; }
     public int CrystalLeague { get; set; }
-    public DateTime Timestamp { get; set; }
+    public DateTime? Timestamp { get; set; }
 }
