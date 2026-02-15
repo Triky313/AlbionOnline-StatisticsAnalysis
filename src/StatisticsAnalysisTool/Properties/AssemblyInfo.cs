88783ad3a6760efc301eb497f6cff5ef59773f17
Aaron <schultzaaron@web.de>
2026-02-15 12:11:58 +0100

    Version increased

--- src/StatisticsAnalysisTool/Properties/AssemblyInfo.cs
+++ src/StatisticsAnalysisTool/Properties/AssemblyInfo.cs
@@ -53,5 +53,5 @@ using System.Runtime.CompilerServices;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // indem Sie "*" wie unten gezeigt eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("8.6.6.*")]
-[assembly: AssemblyFileVersion("8.6.6.0")]
+[assembly: AssemblyVersion("8.6.7.*")]
+[assembly: AssemblyFileVersion("8.6.7.0")]
