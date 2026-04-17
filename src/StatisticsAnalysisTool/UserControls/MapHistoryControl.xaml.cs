5937275ebdc7981fd0ff410a2558752729a8f704
Aaron <schultzaaron@web.de>
2026-04-17 23:32:56 +0200

    Add map history selection and clipboard copy functionality
    Allows users to select specific map history entries and copy their names as a path to the clipboard. Includes support for adding personal notes to map entries and visual indicators for selected items to improve tracking and sharing of map sequences.

--- src/StatisticsAnalysisTool/UserControls/MapHistoryControl.xaml.cs
+++ src/StatisticsAnalysisTool/UserControls/MapHistoryControl.xaml.cs
@@ -1,7 +1,13 @@
+using StatisticsAnalysisTool.Cluster;
 using StatisticsAnalysisTool.Common;
 using StatisticsAnalysisTool.Localization;
 using StatisticsAnalysisTool.Network.Manager;
+using StatisticsAnalysisTool.ViewModels;
 using StatisticsAnalysisTool.Views;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -12,9 +18,16 @@ namespace StatisticsAnalysisTool.UserControls;
 /// </summary>
 public partial class MapHistoryControl
 {
+    private MainWindowViewModel _mainWindowViewModel;
+    private ObservableCollection<ClusterInfo> _enteredCluster;
+
     public MapHistoryControl()
     {
         InitializeComponent();
+
+        Loaded += MapHistoryControl_Loaded;
+        Unloaded += MapHistoryControl_Unloaded;
+        DataContextChanged += MapHistoryControl_DataContextChanged;
     }
 
     private async Task DeleteAllMapHistoryEntriesAsync()
@@ -37,6 +50,166 @@ public partial class MapHistoryControl
         }
 
         await trackingController.ClusterController.ClearMapHistoryAsync();
+    }
+
+    private void CopySelectedMapHistoryToClipboard()
+    {
+        var selectedEntries = _mainWindowViewModel?.EnteredCluster?
+            .Where(x => x.IsSelectedInMapHistory)
+            .OrderBy(x => x.Entered)
+            .Select(x => x.MapHistoryClipboardName)
+            .Where(x => !string.IsNullOrWhiteSpace(x))
+            .ToList();
+
+        if (selectedEntries == null || selectedEntries.Count <= 0)
+        {
+            UpdateCopyButtonState();
+            return;
+        }
+
+        Clipboard.SetText(string.Join(" -> ", selectedEntries));
+    }
+
+    private void MapHistoryControl_Loaded(object sender, RoutedEventArgs e)
+    {
+        AttachToViewModel(DataContext as MainWindowViewModel);
+    }
+
+    private void MapHistoryControl_Unloaded(object sender, RoutedEventArgs e)
+    {
+        DetachFromViewModel();
+    }
+
+    private void MapHistoryControl_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+    {
+        if (ReferenceEquals(e.OldValue, e.NewValue))
+        {
+            return;
+        }
+
+        DetachFromViewModel();
+        AttachToViewModel(e.NewValue as MainWindowViewModel);
+    }
+
+    private void AttachToViewModel(MainWindowViewModel mainWindowViewModel)
+    {
+        if (ReferenceEquals(_mainWindowViewModel, mainWindowViewModel))
+        {
+            UpdateCopyButtonState();
+            return;
+        }
+
+        if (mainWindowViewModel == null)
+        {
+            UpdateCopyButtonState();
+            return;
+        }
+
+        _mainWindowViewModel = mainWindowViewModel;
+        _mainWindowViewModel.PropertyChanged += MainWindowViewModel_PropertyChanged;
+        AttachToCollection(_mainWindowViewModel.EnteredCluster);
+        UpdateCopyButtonState();
+    }
+
+    private void DetachFromViewModel()
+    {
+        DetachFromCollection();
+
+        if (_mainWindowViewModel != null)
+        {
+            _mainWindowViewModel.PropertyChanged -= MainWindowViewModel_PropertyChanged;
+            _mainWindowViewModel = null;
+        }
+
+        UpdateCopyButtonState();
+    }
+
+    private void MainWindowViewModel_PropertyChanged(object sender, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName != nameof(MainWindowViewModel.EnteredCluster))
+        {
+            return;
+        }
+
+        AttachToCollection(_mainWindowViewModel?.EnteredCluster);
+        UpdateCopyButtonState();
+    }
+
+    private void AttachToCollection(ObservableCollection<ClusterInfo> enteredCluster)
+    {
+        if (ReferenceEquals(_enteredCluster, enteredCluster))
+        {
+            return;
+        }
+
+        DetachFromCollection();
+        _enteredCluster = enteredCluster;
+
+        if (_enteredCluster == null)
+        {
+            return;
+        }
+
+        _enteredCluster.CollectionChanged += EnteredCluster_CollectionChanged;
+        foreach (var clusterInfo in _enteredCluster)
+        {
+            clusterInfo.PropertyChanged += ClusterInfo_PropertyChanged;
+        }
+    }
+
+    private void DetachFromCollection()
+    {
+        if (_enteredCluster == null)
+        {
+            return;
+        }
+
+        _enteredCluster.CollectionChanged -= EnteredCluster_CollectionChanged;
+        foreach (var clusterInfo in _enteredCluster)
+        {
+            clusterInfo.PropertyChanged -= ClusterInfo_PropertyChanged;
+        }
+
+        _enteredCluster = null;
+    }
+
+    private void EnteredCluster_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+    {
+        if (e.OldItems != null)
+        {
+            foreach (var item in e.OldItems.OfType<ClusterInfo>())
+            {
+                item.PropertyChanged -= ClusterInfo_PropertyChanged;
+            }
+        }
+
+        if (e.NewItems != null)
+        {
+            foreach (var item in e.NewItems.OfType<ClusterInfo>())
+            {
+                item.PropertyChanged += ClusterInfo_PropertyChanged;
+            }
+        }
+
+        UpdateCopyButtonState();
+    }
+
+    private void ClusterInfo_PropertyChanged(object sender, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == nameof(ClusterInfo.IsSelectedInMapHistory))
+        {
+            UpdateCopyButtonState();
+        }
+    }
+
+    private void UpdateCopyButtonState()
+    {
+        BtnCopySelectedMapHistoryToClipboard.IsEnabled = _mainWindowViewModel?.EnteredCluster?.Any(x => x.IsSelectedInMapHistory) ?? false;
+    }
+
+    private void BtnCopySelectedMapHistoryToClipboard_Click(object sender, RoutedEventArgs e)
+    {
+        CopySelectedMapHistoryToClipboard();
     }
 
     private async void BtnDeleteAllMapHistoryEntries_Click(object sender, RoutedEventArgs e)
