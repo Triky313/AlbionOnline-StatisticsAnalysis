28206b595ebfccb1dd5ce41ae4ee48f30ef2b075
Aaron <schultzaaron@web.de>
2026-05-12 16:23:20 +0200

    Refactor crafting resource list and item grids to use static localized headers
    Streamlines the CraftingControl.xaml ListView column headers by replacing dynamic bindings with direct static references to localization strings, improving readability and performance. Updates the CraftingResourceEntry model formatting without functional changes.

--- src/StatisticsAnalysisTool/Crafting/CraftingResourceEntry.cs
+++ src/StatisticsAnalysisTool/Crafting/CraftingResourceEntry.cs
@@ -9,87 +9,35 @@ namespace StatisticsAnalysisTool.Crafting;
 public class CraftingResourceEntry : BaseViewModel
 {
     [JsonIgnore]
-    public Action ValuesChanged
-    {
-        get;
-        set;
-    }
-
-    public string UniqueName
-    {
-        get;
-        set;
-    }
-
-    public string DisplayName
-    {
-        get;
-        set;
-    }
-
-    public decimal QuantityPerRun
-    {
-        get;
-        set;
-    }
-
-    public double UnitWeight
-    {
-        get;
-        set;
-    }
-
-    public bool IsReturnable
-    {
-        get;
-        set;
-    }
-
-    public decimal? MaxReturnQuantityPerRun
-    {
-        get;
-        set;
-    }
-
-    public CraftingResourceKind ResourceKind
-    {
-        get;
-        set;
-    }
+    public Action ValuesChanged { get; set; }
+    public string UniqueName { get; set; }
+    public string DisplayName { get; set; }
+    public decimal QuantityPerRun { get; set; }
+    public double UnitWeight { get; set; }
+    public bool IsReturnable { get; set; }
+    public decimal? MaxReturnQuantityPerRun { get; set; }
+    public CraftingResourceKind ResourceKind { get; set; }
     public string ResourceKindText
     {
         get
         {
-            switch (ResourceKind)
+            return ResourceKind switch
             {
-                case CraftingResourceKind.Standard:
-                    return LocalizationController.Translation("CRAFTING_RESOURCE_KIND_STANDARD");
-                case CraftingResourceKind.Artefact:
-                    return LocalizationController.Translation("CRAFTING_RESOURCE_KIND_ARTEFACT");
-                case CraftingResourceKind.Favor:
-                    return LocalizationController.Translation("FAVOR");
-                case CraftingResourceKind.Alchemy:
-                    return LocalizationController.Translation("CRAFTING_RESOURCE_KIND_ALCHEMY");
-                case CraftingResourceKind.AvalonianEnergy:
-                    return LocalizationController.Translation("CRAFTING_RESOURCE_KIND_AVALONIAN_ENERGY");
-                case CraftingResourceKind.TomeOfInsight:
-                    return LocalizationController.Translation("CRAFTING_RESOURCE_KIND_TOME_OF_INSIGHT");
-                case CraftingResourceKind.Essence:
-                    return LocalizationController.Translation("CRAFTING_RESOURCE_KIND_ESSENCE");
-                case CraftingResourceKind.Special:
-                    return LocalizationController.Translation("CRAFTING_RESOURCE_KIND_SPECIAL");
-                default:
-                    return ResourceKind.ToString();
-            }
+                CraftingResourceKind.Standard => LocalizationController.Translation("CRAFTING_RESOURCE_KIND_STANDARD"),
+                CraftingResourceKind.Artefact => LocalizationController.Translation("CRAFTING_RESOURCE_KIND_ARTEFACT"),
+                CraftingResourceKind.Favor => LocalizationController.Translation("FAVOR"),
+                CraftingResourceKind.Alchemy => LocalizationController.Translation("CRAFTING_RESOURCE_KIND_ALCHEMY"),
+                CraftingResourceKind.AvalonianEnergy => LocalizationController.Translation("CRAFTING_RESOURCE_KIND_AVALONIAN_ENERGY"),
+                CraftingResourceKind.TomeOfInsight => LocalizationController.Translation("CRAFTING_RESOURCE_KIND_TOME_OF_INSIGHT"),
+                CraftingResourceKind.Essence => LocalizationController.Translation("CRAFTING_RESOURCE_KIND_ESSENCE"),
+                CraftingResourceKind.Special => LocalizationController.Translation("CRAFTING_RESOURCE_KIND_SPECIAL"),
+                _ => ResourceKind.ToString()
+            };
         }
     }
 
     [JsonIgnore]
-    public BitmapImage Icon
-    {
-        get;
-        set;
-    }
+    public BitmapImage Icon { get; set; }
 
     public decimal UnitPrice
     {
