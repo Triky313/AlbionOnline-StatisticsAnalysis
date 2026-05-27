cde2cb2b3174521907a4d3653da5981926211d7a
Aaron <schultzaaron@web.de>
2026-05-27 19:12:45 +0200

    Handle IPv4 packet fragmentation and update default filter
    Implement IPv4 packet reassembly to ensure complete processing of network data that may arrive in fragments. The default packet filter is updated to explicitly capture these fragmented packets, improving data integrity and robustness.

--- src/StatisticsAnalysisTool/ViewModels/SettingsWindowViewModel.cs
+++ src/StatisticsAnalysisTool/ViewModels/SettingsWindowViewModel.cs
@@ -316,7 +316,7 @@ public class SettingsWindowViewModel : BaseViewModel
 
     public void ResetPacketFilter()
     {
-        const string defaultFilter = "(ip or ip6) and (udp and (port 5055 or port 5056 or port 5058))";
+        const string defaultFilter = LibpcapPacketProvider.DefaultPacketFilter;
 
         if (PacketFilter == defaultFilter)
         {
